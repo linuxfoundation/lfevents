3356ff114541069125e82e8b4ac91bcabdd231a2
cjyabraham <cjyabraham@gmail.com>
2019-03-03 14:36:56 +0800

    added filedoc to js file. this should fix the build

--- web/wp-content/mu-plugins/custom/lfevents/public/js/lfevents-public.js
+++ web/wp-content/mu-plugins/custom/lfevents/public/js/lfevents-public.js
@@ -1,4 +1,14 @@
-
+/**
+ * Summary. (use period)
+ *
+ * Description. (use period)
+ *
+ * @link   URL
+ * @file   This files defines the MyClass class.
+ * @author AuthorName.
+ * @since  x.x.x
+ * @package xxx
+ */
 
 (function( $ ) {
 	'use strict';
