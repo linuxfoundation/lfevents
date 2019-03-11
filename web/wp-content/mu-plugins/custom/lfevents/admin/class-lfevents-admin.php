5f49140a0c4e55f2437780e4973ff8f2f4f8545f
cjyabraham <cjyabraham@gmail.com>
2019-03-11 14:41:03 +0800

    fix linting issues

--- web/wp-content/mu-plugins/custom/lfevents/admin/class-lfevents-admin.php
+++ web/wp-content/mu-plugins/custom/lfevents/admin/class-lfevents-admin.php
@@ -104,18 +104,18 @@ class LFEvents_Admin {
 	 */
 	public function new_cpt_events() {
 		$opts = array(
-			'labels'       => array(
+			'labels'        => array(
 				'name'          => __( 'Events' ),
 				'singular_name' => __( 'Event' ),
 			),
-			'public'       => true,
-			'has_archive'  => true,
-			'show_in_rest' => true,
-			'hierarchical' => true,
-			'menu_position'=> 2,
-			'menu_icon'    => 'dashicons-admin-site',
-			'rewrite'      => array( 'slug' => 'events' ),
-			'supports'     => array( 'title', 'editor', 'thumbnail', 'excerpt', 'revisions', 'custom-fields', 'page-attributes' ),
+			'public'        => true,
+			'has_archive'   => true,
+			'show_in_rest'  => true,
+			'hierarchical'  => true,
+			'menu_position' => 2,
+			'menu_icon'     => 'dashicons-admin-site',
+			'rewrite'       => array( 'slug' => 'events' ),
+			'supports'      => array( 'title', 'editor', 'thumbnail', 'excerpt', 'revisions', 'custom-fields', 'page-attributes' ),
 		);
 
 		register_post_type( 'lfevent', $opts );
@@ -142,7 +142,6 @@ class LFEvents_Admin {
 	 * Registers the LFEvent sidebar
 	 *
 	 * @param array $sidebars    Existing sidebars in Gutenberg.
-	 *
 	 */
 	public function create_sidebar( $sidebars ) {
 		// First we define the sidebar with it's tabs, panels and settings.
@@ -161,40 +160,37 @@ class LFEvents_Admin {
 							'label'    => __( 'General Settings' ),
 							'settings' => array(
 								array(
-									'type'            => 'text', // Required.
-									'id'              => 'location',
-									'data_type'       => 'meta', // Available: 'meta', 'none'.
-									'data_key'        => 'location', // Required if 'data_type' is 'meta'.
-									'label'           => __( 'Event location' ),
-									'register_meta'   => true, // This option is applicable only if 'data_type' is 'meta'.
-									'ui_border_top'   => true, // Display CSS border-top in the editor control.
-									// Setting text specific options:
-									'default_value'   => '',
-									'placeholder'     => __( 'City, Country' ),
-									),
+									'type'          => 'text', // Required.
+									'id'            => 'location',
+									'data_type'     => 'meta',
+									'data_key'      => 'location', // Required if 'data_type' is 'meta'.
+									'label'         => __( 'Event location' ),
+									'register_meta' => true, // This option is applicable only if 'data_type' is 'meta'.
+									'ui_border_top' => true, // Display CSS border-top in the editor control.
+									'default_value' => '',
+									'placeholder'   => __( 'City, Country' ),
+								),
 								array(
-									'type'            => 'date_range', // Required.
+									'type'          => 'date_range', // Required.
 									// Optionally, an id may be specified. It will be used by the plugin to
 									// identify the setting and will be applied to the control html.
 									// The prefix set in the sidebar option 'id_prefix' will be applied.
-									'id'              => 'date_range_id',
-									'data_type'       => 'meta', // Available: 'meta', 'localstorage', 'none'.
-									'data_key'        => 'date_range_key', // Required if 'data_type' is 'meta' or 'localstorage'.
+									'id'            => 'date_range_id',
+									'data_type'     => 'meta',
+									'data_key'      => 'date_range_key', // Required if 'data_type' is 'meta' or 'localstorage'.
 									// Use 'data_key_prefix' to set a custom prefix for this setting 'data_key'.
 									// If 'data_key_prefix' is not assigned, the 'data_key_prefix' from the sidebar
 									// where this setting is nested will be used.
-									'label'           => __( 'Event dates', 'my_plugin' ),
-									'register_meta'   => true, // This option is applicable only if 'data_type' is 'meta'.
-									'ui_border_top'   => true, // Display CSS border-top in the editor control.
-									// Setting date_range specific options:
-									'default_value'   => '', // A string with a date that matches 'format'.
-									// To see the available formats
-									// check: http://momentjs.com/docs/#/parsing/string-format/
-									'format'          => 'DD/MM/YYYY',
+									'label'         => __( 'Event dates', 'my_plugin' ),
+									'register_meta' => true, // This option is applicable only if 'data_type' is 'meta'.
+									'ui_border_top' => true, // Display CSS border-top in the editor control.
+									'default_value' => '', // A string with a date that matches 'format'.
+									// To see the available formats check: http://momentjs.com/docs/#/parsing/string-format/.
+									'format'        => 'DD/MM/YYYY',
 									// A string with the locale value.
 									// For example 'en' for english, or 'ja' for japanese.
-									// To see the available locales check https://momentjs.com/
-									'locale'          => 'en',
+									// To see the available locales check https://momentjs.com/.
+									'locale'        => 'en',
 								),
 							),
 						),
@@ -202,16 +198,16 @@ class LFEvents_Admin {
 							'label'    => __( 'Colors' ),
 							'settings' => array(
 								array(
-									'type'            => 'color',
-									'id'              => 'color_id',
-									'data_type'       => 'meta',
-									'data_key'        => 'accent_color', // Required if 'data_type' is 'meta' or 'localstorage'.
-									'label'           => __( 'Accent color' ),
-									'help'            => __( 'Choose a color for all accents for the Event' ),
-									'register_meta'   => true, // This option is applicable only if 'data_type' is 'meta'.
-									'ui_border_top'   => true, // Display CSS border-top in the editor control.
-									'default_value'   => '', // A string with a HEX, rgb or rgba color format.
-									'alpha_control'   => false, // Include alpha control to set color transparency.
+									'type'          => 'color',
+									'id'            => 'color_id',
+									'data_type'     => 'meta',
+									'data_key'      => 'accent_color', // Required if 'data_type' is 'meta' or 'localstorage'.
+									'label'         => __( 'Accent color' ),
+									'help'          => __( 'Choose a color for all accents for the Event' ),
+									'register_meta' => true, // This option is applicable only if 'data_type' is 'meta'.
+									'ui_border_top' => true, // Display CSS border-top in the editor control.
+									'default_value' => '', // A string with a HEX, rgb or rgba color format.
+									'alpha_control' => false, // Include alpha control to set color transparency.
 								),
 							),
 						),
