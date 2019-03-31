6af9edf8db83cec44cdac282e27dc9b476e08eaa
Andy Cochran <acochran@planning.nyc.gov>
2019-03-31 14:57:08 -0400

    change twentynineteen main-menu styles

--- web/wp-content/themes/twentynineteen/inc/template-functions.php
+++ web/wp-content/themes/twentynineteen/inc/template-functions.php
@@ -198,34 +198,34 @@ function twentynineteen_get_discussion_data() {
  * @param object $args      Nav menu args.
  * @return string More link for hidden menu items.
  */
-function twentynineteen_add_ellipses_to_nav( $nav_menu, $args ) {
-
-	if ( 'menu-1' === $args->theme_location ) :
-
-		$nav_menu .= '<div class="main-menu-more">';
-		$nav_menu .= '<ul class="main-menu">';
-		$nav_menu .= '<li class="menu-item menu-item-has-children">';
-		$nav_menu .= '<button class="submenu-expand main-menu-more-toggle is-empty" tabindex="-1" aria-label="More" aria-haspopup="true" aria-expanded="false">';
-		$nav_menu .= '<span class="screen-reader-text">' . esc_html__( 'More', 'twentynineteen' ) . '</span>';
-		$nav_menu .= twentynineteen_get_icon_svg( 'arrow_drop_down_ellipsis' );
-		$nav_menu .= '</button>';
-		$nav_menu .= '<ul class="sub-menu hidden-links">';
-		$nav_menu .= '<li id="menu-item--1" class="mobile-parent-nav-menu-item menu-item--1">';
-		$nav_menu .= '<button class="menu-item-link-return">';
-		$nav_menu .= twentynineteen_get_icon_svg( 'chevron_left' );
-		$nav_menu .= esc_html__( 'Back', 'twentynineteen' );
-		$nav_menu .= '</button>';
-		$nav_menu .= '</li>';
-		$nav_menu .= '</ul>';
-		$nav_menu .= '</li>';
-		$nav_menu .= '</ul>';
-		$nav_menu .= '</div>';
-
-	endif;
-
-	return $nav_menu;
-}
-add_filter( 'wp_nav_menu', 'twentynineteen_add_ellipses_to_nav', 10, 2 );
+// function twentynineteen_add_ellipses_to_nav( $nav_menu, $args ) {
+//
+// 	if ( 'menu-1' === $args->theme_location ) :
+//
+// 		$nav_menu .= '<div class="main-menu-more">';
+// 		$nav_menu .= '<ul class="main-menu">';
+// 		$nav_menu .= '<li class="menu-item menu-item-has-children">';
+// 		$nav_menu .= '<button class="submenu-expand main-menu-more-toggle is-empty" tabindex="-1" aria-label="More" aria-haspopup="true" aria-expanded="false">';
+// 		$nav_menu .= '<span class="screen-reader-text">' . esc_html__( 'More', 'twentynineteen' ) . '</span>';
+// 		$nav_menu .= twentynineteen_get_icon_svg( 'arrow_drop_down_ellipsis' );
+// 		$nav_menu .= '</button>';
+// 		$nav_menu .= '<ul class="sub-menu hidden-links">';
+// 		$nav_menu .= '<li id="menu-item--1" class="mobile-parent-nav-menu-item menu-item--1">';
+// 		$nav_menu .= '<button class="menu-item-link-return">';
+// 		$nav_menu .= twentynineteen_get_icon_svg( 'chevron_left' );
+// 		$nav_menu .= esc_html__( 'Back', 'twentynineteen' );
+// 		$nav_menu .= '</button>';
+// 		$nav_menu .= '</li>';
+// 		$nav_menu .= '</ul>';
+// 		$nav_menu .= '</li>';
+// 		$nav_menu .= '</ul>';
+// 		$nav_menu .= '</div>';
+//
+// 	endif;
+//
+// 	return $nav_menu;
+// }
+// add_filter( 'wp_nav_menu', 'twentynineteen_add_ellipses_to_nav', 10, 2 );
 
 /**
  * WCAG 2.0 Attributes for Dropdown Menus
--- web/wp-content/themes/twentynineteen/template-parts/header/site-branding.php
+++ web/wp-content/themes/twentynineteen/template-parts/header/site-branding.php
@@ -7,6 +7,23 @@
  * @since 1.0.0
  */
 ?>
+
+<?php if ( has_nav_menu( 'menu-1' ) ) : ?>
+	<nav id="site-navigation" class="main-navigation" aria-label="<?php esc_attr_e( 'Top Menu', 'twentynineteen' ); ?>">
+		<input type="checkbox" id="main-menu-toggler" class="main-menu-toggler" />
+		<label for="main-menu-toggler" class="main-menu-toggler-label">&#9776;</label>
+		<?php
+		wp_nav_menu(
+			array(
+				'theme_location' => 'menu-1',
+				'menu_class'     => 'main-menu',
+				'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+			)
+		);
+		?>
+	</nav><!-- #site-navigation -->
+<?php endif; ?>
+
 <div class="site-branding">
 
 	<?php if ( has_custom_logo() ) : ?>
@@ -28,19 +45,6 @@
 			<p class="site-description">
 				<?php echo $description; ?>
 			</p>
-	<?php endif; ?>
-	<?php if ( has_nav_menu( 'menu-1' ) ) : ?>
-		<nav id="site-navigation" class="main-navigation" aria-label="<?php esc_attr_e( 'Top Menu', 'twentynineteen' ); ?>">
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_class'     => 'main-menu',
-					'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
 	<?php endif; ?>
 	<?php if ( has_nav_menu( 'social' ) ) : ?>
 		<nav class="social-navigation" aria-label="<?php esc_attr_e( 'Social Links Menu', 'twentynineteen' ); ?>">
