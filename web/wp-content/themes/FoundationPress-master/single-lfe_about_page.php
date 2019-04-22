a34a1dfde57959692ed8381d8c9d830789c3ba81
cjyabraham <cjyabraham@gmail.com>
2019-04-22 15:41:40 +0700

    changed template names to work for LFEvents

---
+++ web/wp-content/themes/FoundationPress-master/single-lfe_about_page.php
@@ -0,0 +1,54 @@
+<?php //phpcs:ignore
+/**
+ * The template for displaying about pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package FoundationPress
+ * @since FoundationPress 1.0.0
+ */
+
+get_header(); ?>
+
+<?php /* get_template_part( 'template-parts/featured-image' ); */ ?>
+
+<div data-sticky-container>
+	<header class="event-header sticky" data-sticky data-options="marginTop:0;" >
+
+		<div class="pre-nav">
+			<?php
+			if ( $post->post_parent ) {
+				$ancestors = get_post_ancestors( $post->ID );
+				$parent    = $ancestors[ count( $ancestors ) - 1 ];
+			} else {
+				$parent = $post->ID;
+			}
+			echo '<a href="' . post_permalink( $parent ) . '">' . get_the_title( $parent ) . '</a>'; //phpcs:ignore
+			?>
+
+			<button class="menu-toggler button alignright" data-toggle="event-menu">
+				<span class="hamburger-icon"></span>
+			</button>
+		</div>
+
+	</header>
+</div>
+
+<div class="main-container">
+	<div class="main-grid">
+		<main class="main-content-full-width">
+			<?php
+			while ( have_posts() ) :
+				the_post();
+				?>
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+				<?php comments_template(); ?>
+			<?php endwhile; ?>
+		</main>
+		<?php /* get_sidebar(); */ ?>
+	</div>
+</div>
+<?php
+get_footer();
--- web/wp-content/themes/FoundationPress-master/singular.php
+++ web/wp-content/themes/FoundationPress-master/singular.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying pages
+ * The template for displaying pages and all lfevents post types
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages and that
