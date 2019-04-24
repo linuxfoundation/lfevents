f8486f172840fa8bdf49416b1ec63bcc33ad7087
cjyabraham <cjyabraham@gmail.com>
2019-04-24 14:00:04 +0700

    wrote the get_other_events function

--- web/wp-content/themes/FoundationPress-master/library/lfe-functions.php
+++ web/wp-content/themes/FoundationPress-master/library/lfe-functions.php
@@ -24,11 +24,14 @@ function lfe_get_post_types() {
 
 /**
  * Gets related LFEvents for current post.  Only returns Events for the current year.
+ *
+ * @return array Returns array of IDs of posts that are related.
  */
 function lfe_get_related_events() {
 	global $post;
-	$post_id_in = $post->ID;
-	$terms = wp_get_post_terms( $post_id_in, 'lfevent-category', array( 'fields' => 'ids' ) );
+	$related_events = [];
+
+	$terms = wp_get_post_terms( $post->ID, 'lfevent-category', array( 'fields' => 'ids' ) );
 
 	$args = array(
 		'post_type'   => 'page',
@@ -36,6 +39,7 @@ function lfe_get_related_events() {
 		'no_found_rows' => true,  // used to improve performance.
 		'update_post_meta_cache' => false, // used to improve performance.
 		'update_post_term_cache' => false, // used to improve performance.
+		'post__not_in' => array( $post->ID ), // ignores current post.
 		'tax_query'   => array(
 			array(
 				'taxonomy' => 'lfevent-category',
@@ -48,24 +52,14 @@ function lfe_get_related_events() {
 	$the_query = new WP_Query( $args );
 
 	if ( $the_query->have_posts() ) {
-		echo '<li class="page_item page_item_has_children">';
-		echo '<a href="' . esc_url( home_url( '/' ) ) . '">Other Events</a>';
-		echo '<ul class="children">';
-
 		while ( $the_query->have_posts() ) {
 			$the_query->the_post();
-			if ( $post->ID !== $post_id_in ) { // don't list the current post.
-				echo '<li><a href="' . esc_url( get_the_permalink() ) . '">' . esc_html( get_the_title() ) . '</a></li>';
-			}
+			$related_events[] = array( 'ID' => get_the_ID() );
 		}
-
-		echo '</ul>';
-		echo '</li>';
-
-		wp_reset_postdata(); // Restore original Post Data.
-	} else {
-		echo '<li class="page_item"><a href="' . esc_url( home_url( '/' ) ) . '">Other Events</a></li>';
 	}
+	wp_reset_postdata(); // Restore original Post Data.
+
+	return $related_events;
 
 }
 
@@ -87,25 +81,44 @@ function lfe_get_archive() {
 			AND id <> %d
 			ORDER BY post_type ASC",
 			'lfevent%',
-			$post->post_title,
+			$post->post_name,
 			$post->ID
 		)
 	);
 
-	if ( $myposts ) {
+	return $myposts;
+}
+
+/**
+ * Generates the "Other Events" menu item.
+ */
+function lfe_get_other_events() {
+	$related_events = lfe_get_related_events();
+	$archive_events = lfe_get_archive();
+
+	if ( $related_events || $archive_events ) {
 		echo '<li class="page_item page_item_has_children">';
-		echo '<a href="' . esc_url( home_url( '/' ) ) . '">Archive</a>';
+	} else {
+		echo '<li class="page_item">';
+	}
+
+	echo '<a href="' . esc_url( home_url( '/' ) ) . '">Other Events</a>';
+
+	if ( $related_events || $archive_events ) {
 		echo '<ul class="children">';
 
-		foreach ( $myposts as $p ) {
-			echo '<li><a href="' . esc_url( get_permalink( $p->ID ) ) . '">' . esc_html( get_post_type( $p->ID ) . ' - ' . get_the_title() ) . '</a></li>';
+		foreach ( $related_events as $p ) {
+			echo '<li><a href="' . esc_url( get_permalink( $p['ID'] ) ) . '">Related: ' . esc_html( get_post_type( $p['ID'] ) . ' - ' . get_the_title( $p['ID'] ) ) . '</a></li>';
+		}
+
+		foreach ( $archive_events as $p ) {
+			echo '<li><a href="' . esc_url( get_permalink( $p->ID ) ) . '">Archive: ' . esc_html( get_post_type( $p->ID ) . ' - ' . get_the_title( $p->ID ) ) . '</a></li>';
 		}
 
 		echo '</ul>';
-		echo '</li>';
 	}
 
-	return $myposts;
+	echo '</li>';
 }
 
 
