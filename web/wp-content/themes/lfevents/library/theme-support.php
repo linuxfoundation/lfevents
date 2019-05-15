00c2f5283c7fd1628eaf266e3bce37a7f112955a
Andy Cochran <acochran@planning.nyc.gov>
2019-05-14 23:48:51 -0400

    prevent editor styles clobbering wp-block styles

--- web/wp-content/themes/lfevents/library/theme-support.php
+++ web/wp-content/themes/lfevents/library/theme-support.php
@@ -49,10 +49,8 @@ if ( ! function_exists( 'foundationpress_theme_support' ) ) :
 
 		add_theme_support( 'editor-styles' );
 
-		add_editor_style( get_stylesheet_directory_uri() . '/dist/assets/css/editor.css' );
-
 		// Add foundation.css as editor style https://codex.wordpress.org/Editor_Style.
-		// add_editor_style( 'dist/assets/css/' . foundationpress_asset_path( 'editor.css' ) ); //phpcs:ignore.
+		add_editor_style( 'dist/assets/css/' . foundationpress_asset_path( 'editor.css' ) ); //phpcs:ignore.
 	}
 
 	add_action( 'after_setup_theme', 'foundationpress_theme_support' );
