52f614835ebb2cadcc236555bca035264b0f855c
cjyabraham <cjyabraham@gmail.com>
2019-06-03 17:56:36 +0700

    got the recursion working

--- web/wp-content/plugins/deep-duplicate-page/deep-duplicate-page.php
+++ web/wp-content/plugins/deep-duplicate-page/deep-duplicate-page.php
@@ -22,93 +22,22 @@ if ( ! defined( 'WPINC' ) ) {
  * Function creates page deep duplicate
  */
 function ddp_deep_duplicate_page() {
-	global $wpdb;
 	if ( ! ( isset( $_GET['post'] ) || isset( $_POST['post'] ) || ( isset( $_REQUEST['action'] ) && 'ddp_deep_duplicate_page' == $_REQUEST['action'] ) ) ) {
 		wp_die( 'No page to duplicate has been supplied!' );
 	}
 
-	/*
-	 * Nonce verification
-	 */
+	// Nonce verification.
 	if ( ! isset( $_GET['deep_duplicate_nonce'] ) || ! wp_verify_nonce( $_GET['deep_duplicate_nonce'], basename( __FILE__ ) ) ) { //phpcs:ignore
 		return;
 	}
 
-	/*
-	 * get the original post id
-	 */
+	// get the original post id.
 	$post_id = ( isset( $_GET['post'] ) ? absint( $_GET['post'] ) : absint( $_POST['post'] ) );
 
-	/*
-	 * and all the original post data then
-	 */
-	$post = get_post( $post_id );
+	$new_post_id = deep_duplicate( $post_id, 0 );
 
-	/*
-	 * if you don't want current user to be the new post author,
-	 * then change next couple of lines to this: $new_post_author = $post->post_author;
-	 */
-	$current_user = wp_get_current_user();
-	$new_post_author = $current_user->ID;
-
-	/*
-	 * if post data exists, create the post duplicate
-	 */
-	if ( isset( $post ) && null !== $post ) {
-		/*
-		 * new post data array
-		 */
-		$args = array(
-			'comment_status' => $post->comment_status,
-			'ping_status'    => $post->ping_status,
-			'post_author'    => $new_post_author,
-			'post_content'   => $post->post_content,
-			'post_excerpt'   => $post->post_excerpt,
-			'post_name'      => $post->post_name,
-			'post_parent'    => $post->post_parent,
-			'post_password'  => $post->post_password,
-			'post_status'    => 'draft',
-			'post_title'     => $post->post_title,
-			'post_type'      => $post->post_type,
-			'to_ping'        => $post->to_ping,
-			'menu_order'     => $post->menu_order,
-		);
-
-		/*
-		 * insert the post by wp_insert_post() function
-		 */
-		$new_post_id = wp_insert_post( $args );
-
-		/*
-		 * get all current post terms ad set them to the new post draft
-		 */
-		$taxonomies = get_object_taxonomies( $post->post_type ); // returns array of taxonomy names for post type, ex array("category", "post_tag");.
-		foreach ( $taxonomies as $taxonomy ) {
-			$post_terms = wp_get_object_terms( $post_id, $taxonomy, array( 'fields' => 'slugs' ) );
-			wp_set_object_terms( $new_post_id, $post_terms, $taxonomy, false );
-		}
-
-		/*
-		 * duplicate all post meta just in two SQL queries
-		 */
-		$post_meta_infos = $wpdb->get_results( $wpdb->prepare( "SELECT meta_key, meta_value FROM $wpdb->postmeta WHERE post_id=%d", $post_id ) );
-		if ( count( $post_meta_infos ) != 0 ) {
-			$sql_query = "INSERT INTO $wpdb->postmeta (post_id, meta_key, meta_value) ";
-			foreach ( $post_meta_infos as $meta_info ) {
-				$meta_key = $meta_info->meta_key;
-				if ( '_wp_old_slug' == $meta_key ) {
-					continue;
-				}
-				$meta_value = addslashes( $meta_info->meta_value );
-				$sql_query_sel[] = "SELECT $new_post_id, '$meta_key', '$meta_value'";
-			}
-			$sql_query .= implode( ' UNION ALL ', $sql_query_sel );
-			$wpdb->query( $sql_query ); //phpcs:ignore
-		}
-
-		/*
-		 * finally, redirect to the edit post screen for the new draft
-		 */
+	if ( $new_post_id ) {
+		// finally, redirect to the edit post screen for the new draft.
 		wp_redirect( admin_url( 'post.php?action=edit&post=' . $new_post_id ) );
 		exit;
 	} else {
@@ -131,3 +60,87 @@ function ddp_duplicate_page_link( $actions, $post ) {
 }
 
 add_filter( 'page_row_actions', 'ddp_duplicate_page_link', 10, 2 );
+
+/**
+ * The recursive function that traverses the children of a page and duplicates each of them
+ * Returns the new post id that was created.
+ *
+ * @param int $post_id the post id.
+ * @param int $new_parent_id the new parent id for the duplicated post.
+ */
+function deep_duplicate( $post_id, $new_parent_id ) {
+	global $wpdb;
+
+	$post_in = get_post( $post_id );
+
+	if ( ! isset( $post_in ) || null === $post_in ) {
+		return null;
+	}
+
+	/*
+	 * if you don't want current user to be the new post author,
+	 * then change next couple of lines to this: $new_post_author = $post_in->post_author;
+	 */
+	$current_user_in = wp_get_current_user();
+	$new_post_author = $current_user_in->ID;
+
+	// new post data array.
+	$args = array(
+		'comment_status' => $post_in->comment_status,
+		'ping_status'    => $post_in->ping_status,
+		'post_author'    => $new_post_author,
+		'post_content'   => $post_in->post_content,
+		'post_excerpt'   => $post_in->post_excerpt,
+		'post_name'      => $post_in->post_name,
+		'post_parent'    => $new_parent_id,
+		'post_password'  => $post_in->post_password,
+		'post_status'    => 'draft',
+		'post_title'     => $post_in->post_title,
+		'post_type'      => $post_in->post_type,
+		'to_ping'        => $post_in->to_ping,
+		'menu_order'     => $post_in->menu_order,
+	);
+
+	// insert the post by wp_insert_post() function.
+	$new_post_id = wp_insert_post( $args );
+
+
+	// get all current post terms ad set them to the new post draft.
+	$taxonomies = get_object_taxonomies( $post_in->post_type ); // returns array of taxonomy names for post type, ex array("category", "post_tag");.
+	foreach ( $taxonomies as $taxonomy_in ) {
+		$post_terms = wp_get_object_terms( $post_id, $taxonomy_in, array( 'fields' => 'slugs' ) );
+		wp_set_object_terms( $new_post_id, $post_terms, $taxonomy_in, false );
+	}
+
+	// duplicate all post meta just in two SQL queries.
+	$post_meta_infos = $wpdb->get_results( $wpdb->prepare( "SELECT meta_key, meta_value FROM $wpdb->postmeta WHERE post_id=%d", $post_id ) );
+	if ( count( $post_meta_infos ) != 0 ) {
+		$sql_query = "INSERT INTO $wpdb->postmeta (post_id, meta_key, meta_value) ";
+		foreach ( $post_meta_infos as $meta_info ) {
+			$meta_key = $meta_info->meta_key;
+			if ( '_wp_old_slug' == $meta_key ) {
+				continue;
+			}
+			$meta_value = addslashes( $meta_info->meta_value );
+			$sql_query_sel[] = "SELECT $new_post_id, '$meta_key', '$meta_value'";
+		}
+		$sql_query .= implode( ' UNION ALL ', $sql_query_sel );
+		$wpdb->query( $sql_query ); //phpcs:ignore
+	}
+
+	$args = array(
+		'post_parent' => $post_id,
+		'post_type'   => $post_in->post_type,
+		'numberposts' => -1,
+		'post_status' => 'any',
+	);
+	$children = get_children( $args );
+
+	if ( $children ) {
+		foreach ( $children as $child ) {
+			deep_duplicate( $child->ID, $new_post_id );
+		}
+	}
+
+	return $new_post_id;
+}
