d6fa6564f711f0bf7a16084c2d2b624c2e780472
cjyabraham <cjyabraham@gmail.com>
2019-06-04 11:01:28 +0700

    added some styles to the admin to make the clone page tree button look good

--- web/wp-content/mu-plugins/custom/lfevents/includes/class-lfevents.php
+++ web/wp-content/mu-plugins/custom/lfevents/includes/class-lfevents.php
@@ -153,7 +153,7 @@ class LFEvents {
 
 		$plugin_admin = new LFEvents_Admin( $this->get_lfevents(), $this->get_version() );
 
-		// $this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
+		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		// $this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
 		$this->loader->add_action( 'init', $plugin_admin, 'new_cpts' );
 		$this->loader->add_action( 'init', $plugin_admin, 'register_event_categories' );
