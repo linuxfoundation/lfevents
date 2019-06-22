96dba7751fb6bd791979a824f2bdf2eb1f5f5665
Andy Cochran <acochran@planning.nyc.gov>
2019-06-22 17:18:34 -0400

    add a header menu for About Pages

--- web/wp-content/themes/lfevents/page-templates/upcoming-events.php
+++ web/wp-content/themes/lfevents/page-templates/upcoming-events.php
@@ -8,6 +8,21 @@
 
 get_header(); ?>
 
+<div data-sticky-container>
+	<header class="main-header sticky" data-sticky data-sticky-on="large" data-options="marginTop:0;">
+
+		<button class="menu-toggler button alignright hide-for-large" data-toggle="main-menu">
+			<span class="hamburger-icon"></span>
+		</button>
+
+		<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/' . foundationpress_asset_path( 'logo_lfe_blue.png' ); ?>"></a>
+
+		<nav id="main-menu" class="main-menu show-for-large" data-toggler="show-for-large" role="navigation">
+			<?php foundationpress_about_pages_nav(); ?>
+		</nav>
+	</header>
+</div>
+
 <div class="main-container">
 	<div class="main-grid">
 		<header>
