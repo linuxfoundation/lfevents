10f290051e9a31cbe433a733c406a72f2029ab51
Andy Cochran <acochran@planning.nyc.gov>
2019-06-22 15:27:53 -0400

    nix footer widgets

--- web/wp-content/themes/lfevents/library/widget-areas.php
+++ web/wp-content/themes/lfevents/library/widget-areas.php
@@ -24,17 +24,17 @@ if ( ! function_exists( 'foundationpress_sidebar_widgets' ) ) :
 			)
 		);
 
-		register_sidebar(
-			array(
-				'id'            => 'footer-widgets',
-				'name'          => __( 'Footer widgets', 'foundationpress' ),
-				'description'   => __( 'Drag widgets to this footer container', 'foundationpress' ),
-				'before_widget' => '<section id="%1$s" class="widget %2$s">',
-				'after_widget'  => '</section>',
-				'before_title'  => '<h6>',
-				'after_title'   => '</h6>',
-			)
-		);
+		// register_sidebar(
+		// 	array(
+		// 		'id'            => 'footer-widgets',
+		// 		'name'          => __( 'Footer widgets', 'foundationpress' ),
+		// 		'description'   => __( 'Drag widgets to this footer container', 'foundationpress' ),
+		// 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		// 		'after_widget'  => '</section>',
+		// 		'before_title'  => '<h6>',
+		// 		'after_title'   => '</h6>',
+		// 	)
+		// );
 	}
 
 	add_action( 'widgets_init', 'foundationpress_sidebar_widgets' );
