6e81a53f4ebf0b22e8233322be60e36a16be4a20
Andy Cochran <acochran@planning.nyc.gov>
2019-06-29 16:45:01 -0400

    simpler speaker design, use vars for hard-coded speaker styles

--- web/wp-content/plugins/speakers-block/src/init.php
+++ web/wp-content/plugins/speakers-block/src/init.php
@@ -87,13 +87,24 @@ add_action( 'init', 'speakers_block_cgb_block_assets' );
  */
 function speakers_block_callback( $attributes, $content ) {
 	global $post;
-	
+
 	$speakers_to_show = explode( ',', $attributes['speakers'] );
 	if ( ! $speakers_to_show ) {
 		return '';
 	}
 
-	$out = '<section class="speakers-section wp-block-ugb-container alignfull" style="background: linear-gradient(90deg, #6C3483 0%, #2874A6 100%); color: white;"><h3 class="speakers-section-title">Featured Speakers</h3><ul class="speaker-list grid-x">';
+	$bg_color_1 = 'rgb(123, 10, 67)'; // color should be set in wp-admin.
+	$bg_color_2 = 'rgb(98, 42, 101)'; // color should be set in wp-admin.
+	$white_text = true; // boolean should be set in wp-admin.
+	if ( $white_text ) {
+		$text_color = 'rgb(255,255,255)';
+		$gradient_color = 'rgba(255,255,255,0.15)';
+	} else {
+		$text_color = 'rgb(33,35,38)';
+		$gradient_color = 'rgba(33,35,38,0.15)';
+	}
+
+	$out = '<section class="speakers-section wp-block-ugb-container alignfull" style="background: linear-gradient(90deg, ' . $bg_color_1 . ' 0%, ' . $bg_color_2 . ' 100%); color: ' . $text_color . ';"><h3 class="speakers-section-title">Featured Speakers</h3><ul class="speaker-list grid-x">';
 
 	foreach ( $speakers_to_show as $speaker ) {
 		$the_query = new WP_Query(
@@ -108,7 +119,7 @@ function speakers_block_callback( $attributes, $content ) {
 		if ( $the_query->have_posts() ) {
 			while ( $the_query->have_posts() ) {
 				$the_query->the_post();
-				$out .= '<li id="speaker-' . get_the_id() . '" class="speaker cell small-6 medium-4 xxlarge-3" data-toggler=".open" style="background: linear-gradient(-45deg, #6C3483 0%, #2874A6 100%);">';
+				$out .= '<li id="speaker-' . get_the_id() . '" class="speaker cell small-6 medium-4 xxlarge-3" data-toggler=".open" style="background: linear-gradient(-45deg, transparent 30%, ' . $gradient_color . ' 100%);">';
 				$out .= '	<div class="grid-x">';
 				$out .= '		<div class="cell large-5">';
 				$out .= '			<div class="headshot" style="background-image:url(' . get_the_post_thumbnail_url() . ');" data-toggle="speaker-' . get_the_id() . '">';
@@ -125,13 +136,13 @@ function speakers_block_callback( $attributes, $content ) {
 				$website = get_post_meta( $post->ID, 'lfes_website', true );
 				$out .= '			<ul class="social-media-links">';
 				if ( $twitter ) {
-					$out .= '				<li><a href="' . $twitter . '"><svg class="social-icon--twitter" style="fill:white;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"/></svg></a></li>';
+					$out .= '				<li><a href="' . $twitter . '"><svg class="social-icon--twitter" style="fill:' . $text_color . ';" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"/></svg></a></li>';
 				}
 				if ( $linkedin ) {
-					$out .= '				<li><a href="' . $linkedin . '"><svg class="social-icon--linkedin" style="fill:white;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"/></svg></a></li>';					
+					$out .= '				<li><a href="' . $linkedin . '"><svg class="social-icon--linkedin" style="fill:' . $text_color . ';" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"/></svg></a></li>';
 				}
 				if ( $website ) {
-					$out .= '				<li><a href="' . $website . '"><svg class="social-icon--website" style="fill:white;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" style="fill:white;"><path d="M448 80v352c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48V80c0-26.51 21.49-48 48-48h352c26.51 0 48 21.49 48 48zm-88 16H248.029c-21.313 0-32.08 25.861-16.971 40.971l31.984 31.987L67.515 364.485c-4.686 4.686-4.686 12.284 0 16.971l31.029 31.029c4.687 4.686 12.285 4.686 16.971 0l195.526-195.526 31.988 31.991C358.058 263.977 384 253.425 384 231.979V120c0-13.255-10.745-24-24-24z"/></svg></a></li>';					
+					$out .= '				<li><a href="' . $website . '"><svg class="social-icon--website" style="fill:' . $text_color . ';" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M448 80v352c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48V80c0-26.51 21.49-48 48-48h352c26.51 0 48 21.49 48 48zm-88 16H248.029c-21.313 0-32.08 25.861-16.971 40.971l31.984 31.987L67.515 364.485c-4.686 4.686-4.686 12.284 0 16.971l31.029 31.029c4.687 4.686 12.285 4.686 16.971 0l195.526-195.526 31.988 31.991C358.058 263.977 384 253.425 384 231.979V120c0-13.255-10.745-24-24-24z"/></svg></a></li>';
 				}
 				$out .= '			</ul>';
 				$out .= '		</div>';
