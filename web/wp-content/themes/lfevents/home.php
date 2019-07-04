03ffe7c42ee5fbe323ec71cf07de42ce4bb79458
cjyabraham <cjyabraham@gmail.com>
2019-07-04 13:26:44 +0700

    got main contents of homepage in place

--- web/wp-content/themes/lfevents/home.php
+++ web/wp-content/themes/lfevents/home.php
@@ -27,6 +27,7 @@ get_header(); ?>
 		<main class="main-content-full-width">
 
 			<?php
+			// Top homepage content.
 			$query = new WP_Query(
 				array(
 					'post_type' => 'lfe_about_page',
@@ -44,8 +45,82 @@ get_header(); ?>
 
 			<hr>
 
-			[list of events]
+			<?php
+			// Upcoming Events.
+			$query = new WP_Query(
+				array(
+					'post_type' => 'page',
+					'post_parent' => 0,
+					'no_found_rows' => true,
+					'meta_key'   => 'lfes_date_start',
+					'orderby'    => 'meta_value',
+					'order'      => 'ASC',
+					'post_status' => array( 'draft', 'publish' ),
+				)
+			);
+			if ( $query->have_posts() ) {
+				while ( $query->have_posts() ) {
+					$query->the_post();
+					$dt_date_start = new DateTime( get_post_meta( $post->ID, 'lfes_date_start', true ) );
+					$dt_date_end = new DateTime( get_post_meta( $post->ID, 'lfes_date_end', true ) );
+					?>
+					<article id="post-<?php the_ID(); ?>" class="cell medium-6 large-4">
+						<div class="callout">
+							<h4 class="h5 no-margin"><strong>
+							<?php
+							if ( 'publish' == $post->post_status ) {
+								echo '<a href="' . get_the_permalink() . '">' . get_the_title() . '</a>'; //phpcs:ignore
+							} else {
+								the_title();
+							}
+							?>
+							</strong></h4>
+							<p>
+								<?php
+								echo '<small>';
+								if ( $dt_date_start != $dt_date_end ) {
+									echo esc_html( $dt_date_start->format( 'm/j/Y' ) . ' - ' . $dt_date_end->format( 'm/j/Y' ) );
+								} else {
+									echo esc_html( $dt_date_start->format( 'm/j/Y' ) );
+								}
+								$country = wp_get_post_terms( $post->ID, 'lfevent-country' );
+								if ( $country ) {
+									$country = $country[0]->name;
+									echo ' | ' . esc_html( get_post_meta( $post->ID, 'lfes_city', true ) ) . ', ' . esc_html( $country );
+								}
+								echo '</small>';
+								?>
+							</p>
+						</div>
+					</article>
+					<?php
+				}
+				wp_reset_postdata();
+			}
+			?>
+			<a href="<?php echo esc_url( home_url( '/about/events-calendar' ) ); ?>">Full Calls for Proposals Calendar</a>
 
+			<h2>Latest News</h2>
+			<?php
+			// Latest News.
+			$query = new WP_Query(
+				array(
+					'post_type' => 'post',
+					'no_found_rows' => true,
+					'numberposts' => 5,
+				)
+			);
+			if ( $query->have_posts() ) {
+				while ( $query->have_posts() ) {
+					$query->the_post();
+					echo '<p><a href="' . get_permalink() . '">' . get_the_title() .'</a></p>';
+					echo '<p>' . get_the_date() . '</p>';
+					echo '<p>' . get_the_excerpt() . '</p>';
+				}
+			}
+			wp_reset_postdata();
+			?>
+			<a href="<?php echo esc_url( home_url( '/about/news' ) ); ?>">Read More News</a>
 		</main>
 	</div>
 </div>
