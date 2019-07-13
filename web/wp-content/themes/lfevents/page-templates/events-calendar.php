0c86c801ea8d7f296e4188f01f4278563b86826c
Andy Cochran <acochran@planning.nyc.gov>
2019-07-13 18:45:17 -0400

    refactor about page header,contaner,default widths

--- web/wp-content/themes/lfevents/page-templates/events-calendar.php
+++ web/wp-content/themes/lfevents/page-templates/events-calendar.php
@@ -10,19 +10,23 @@ get_header();
 get_template_part( 'template-parts/global-nav' );
 ?>
 
-<div class="main-container about-page">
-	<div class="main-grid grid-container">
+<div class="main-container">
+	<div class="main-grid">
 		<main class="main-content-full-width">
 			<?php
 			while ( have_posts() ) :
 				the_post();
 				?>
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<header>
-						<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-					</header>
-					<div class="event-calendar-container">
-						<?php the_content(); ?>
+					<div class="entry-content">
+						<header class="alignwide about-page-header">
+							<h1 class="entry-title"><?php the_title(); ?></h1>
+						</header>
+						<div class="alignwide">
+							<div class="event-calendar-container">
+								<?php the_content(); ?>
+							</div>
+						</div>
 					</div>
 				</article>
 			<?php endwhile; ?>
--- web/wp-content/themes/lfevents/template-parts/content-about.php
+++ web/wp-content/themes/lfevents/template-parts/content-about.php
@@ -10,6 +10,9 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<div class="entry-content">
+		<header class="alignwide about-page-header">
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+		</header>
 		<?php the_content(); ?>
 		<?php edit_post_link( __( '(Edit)', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
 	</div>
