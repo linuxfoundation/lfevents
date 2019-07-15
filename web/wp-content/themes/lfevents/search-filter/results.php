455a96ec89620c1f4ad65853aab60bf6cb6bcfab
cjyabraham <cjyabraham@gmail.com>
2019-07-15 18:41:35 +0700

    move back to doing archive views via a button; sort the past events in the opposite way

--- web/wp-content/themes/lfevents/search-filter/results.php
+++ web/wp-content/themes/lfevents/search-filter/results.php
@@ -28,6 +28,11 @@ if ( $query->have_posts() ) {
 	$month = 0;
 	while ( $query->have_posts() ) {
 		$query->the_post();
+		if ( 'page' == $post->post_type ) {
+			$is_upcoming_events = true;
+		} else {
+			$is_upcoming_events = false;
+		}
 		$dt_date_start = new DateTime( get_post_meta( $post->ID, 'lfes_date_start', true ) );
 		$dt_date_end = new DateTime( get_post_meta( $post->ID, 'lfes_date_end', true ) );
 		$cfp_date_start = get_post_meta( $post->ID, 'lfes_cfp_date_start', true );
@@ -36,14 +41,26 @@ if ( $query->have_posts() ) {
 		$dt_cfp_date_end = new DateTime( $cfp_date_end );
 		$cfp_active = get_post_meta( $post->ID, 'lfes_cfp_active', true );
 
-		if ( ( 0 == $y ) || ( $y < (int) $dt_date_start->format( 'Y' ) ) ) {
-			$y = (int) $dt_date_start->format( 'Y' );
-			echo '<h2 class="cell event-calendar-year">' . esc_html( $y ) . '</h2>';
-			$month = (int) $dt_date_start->format( 'm' );
-			echo '<h3 class="cell event-calendar-month">' . esc_html( $dt_date_start->format( 'F' ) ) . '</h3>';
-		} elseif ( ( 0 == $month ) || ( $month < (int) $dt_date_start->format( 'm' ) ) ) {
-			$month = (int) $dt_date_start->format( 'm' );
-			echo '<h3 class="cell event-calendar-month">' . esc_html( $dt_date_start->format( 'F' ) ) . '</h3>';
+		if ( $is_upcoming_events ) {
+			if ( ( 0 == $y ) || ( $y < (int) $dt_date_start->format( 'Y' ) ) ) {
+				$y = (int) $dt_date_start->format( 'Y' );
+				echo '<h2 class="cell event-calendar-year">' . esc_html( $y ) . '</h2>';
+				$month = (int) $dt_date_start->format( 'm' );
+				echo '<h3 class="cell event-calendar-month">' . esc_html( $dt_date_start->format( 'F' ) ) . '</h3>';
+			} elseif ( ( 0 == $month ) || ( $month < (int) $dt_date_start->format( 'm' ) ) ) {
+				$month = (int) $dt_date_start->format( 'm' );
+				echo '<h3 class="cell event-calendar-month">' . esc_html( $dt_date_start->format( 'F' ) ) . '</h3>';
+			}
+		} else {
+			if ( ( 0 == $y ) || ( $y > (int) $dt_date_start->format( 'Y' ) ) ) {
+				$y = (int) $dt_date_start->format( 'Y' );
+				echo '<h2 class="cell event-calendar-year">' . esc_html( $y ) . '</h2>';
+				$month = (int) $dt_date_start->format( 'm' );
+				echo '<h3 class="cell event-calendar-month">' . esc_html( $dt_date_start->format( 'F' ) ) . '</h3>';
+			} elseif ( ( 0 == $month ) || ( $month > (int) $dt_date_start->format( 'm' ) ) ) {
+				$month = (int) $dt_date_start->format( 'm' );
+				echo '<h3 class="cell event-calendar-month">' . esc_html( $dt_date_start->format( 'F' ) ) . '</h3>';
+			}
 		}
 		?>
 		<article id="post-<?php the_ID(); ?>" class="cell medium-6 callout">
@@ -70,18 +87,21 @@ if ( $query->have_posts() ) {
 					echo ' | ' . esc_html( get_post_meta( $post->ID, 'lfes_city', true ) ) . ', ' . esc_html( $country );
 				}
 				echo '</small>';
-				echo '<br />CFP Status: ';
 
-				if ( '0' === $cfp_active ) {
-					echo 'No Call for Proposals';
-				} elseif ( ! ( $cfp_date_start ) ) {
-					echo 'Details Coming Soon';
-				} elseif ( strtotime( $cfp_date_end ) < time() ) {
-					echo 'Closed';
-				} elseif ( strtotime( $cfp_date_end ) > time() && strtotime( $cfp_date_start ) < time() ) {
-					echo 'Closes ' . esc_html( $dt_cfp_date_end->format( 'l, F j, Y' ) );
-				} elseif ( strtotime( $cfp_date_end ) > time() && strtotime( $cfp_date_start ) > time() ) {
-					echo 'Opens ' . esc_html( $dt_cfp_date_start->format( 'l, F j, Y' ) );
+				if ( $is_upcoming_events ) {
+					echo '<br />CFP Status: ';
+
+					if ( '0' === $cfp_active ) {
+						echo 'No Call for Proposals';
+					} elseif ( ! ( $cfp_date_start ) ) {
+						echo 'Details Coming Soon';
+					} elseif ( strtotime( $cfp_date_end ) < time() ) {
+						echo 'Closed';
+					} elseif ( strtotime( $cfp_date_end ) > time() && strtotime( $cfp_date_start ) < time() ) {
+						echo 'Closes ' . esc_html( $dt_cfp_date_end->format( 'l, F j, Y' ) );
+					} elseif ( strtotime( $cfp_date_end ) > time() && strtotime( $cfp_date_start ) > time() ) {
+						echo 'Opens ' . esc_html( $dt_cfp_date_start->format( 'l, F j, Y' ) );
+					}
 				}
 				?>
 			</p>
@@ -98,29 +118,19 @@ echo '</div>';
 // this controls the appearence and behavior of the link to navigate between upcoming and past events.
 $( document ).ready( function() {
 	if ( $( '#switch-archive-view' ).length === 0 ) {
-		$( '.event-calendar-container form' ).prepend( `
-			<a class="" id="switch-archive-view" href="#">
-				<span class="switch tiny">
-					<input class="switch-input" type="checkbox" name="switch-archive-view-input" id="switch-archive-view-input">
-					<label class="switch-paddle" for="switch-archive-view-input"><span class="show-for-sr">Toggle Past Events</span></label>
-				</span>
-				<label id="switch-archive-view-label" for="switch-archive-view-input"></label>
-			</a>
-		` );
+		$( '.event-calendar-container form' ).prepend( '<a class="button expanded" id="switch-archive-view" href="#"></a>' );
 	}
 	var currentUrl = $( location ).attr( 'href' )
 	if ( -1 == currentUrl.indexOf( 'events-calendar/archive' ) ) {
 		//we are on the event-calendar page.
 		newUrl = currentUrl.replace( 'events-calendar', 'events-calendar/archive' );
 		$( '#switch-archive-view' ).attr( "href", newUrl );
-		$( '#switch-archive-view-label' ).html( 'Show Past Events' );
-		$( '#switch-archive-view-input' ).prop('checked', false);
+		$( '#switch-archive-view' ).html( 'Show Past Events' );
 	} else {
 		//we are on the event-calendar/archive page.
 		newUrl = currentUrl.replace( 'events-calendar/archive', 'events-calendar' );
 		$( '#switch-archive-view' ).attr( "href", newUrl );
-		$( '#switch-archive-view-label' ).html( 'Show Upcoming Events' );
-		$( '#switch-archive-view-input' ).prop('checked', true);
+		$( '#switch-archive-view' ).html( 'Show Upcoming Events' );
 	}
 });
 </script>
