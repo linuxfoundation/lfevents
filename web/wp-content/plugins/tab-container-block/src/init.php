5a06736bbc2bf2210469e62cb37170a83372de3d
cjyabraham <cjyabraham@gmail.com>
2019-08-29 14:30:42 +0700

    got the menu generating by using regex to parse the content of the page

--- web/wp-content/plugins/tab-container-block/src/init.php
+++ web/wp-content/plugins/tab-container-block/src/init.php
@@ -97,11 +97,26 @@ add_action( 'init', 'tab_container_block_cgb_block_assets' );
  */
 function tcb_content_filter( $content ) {
 	// get all div tags of class "wp-block-cgb-block-tab-container-block".
+	$tag_regex = '/<[^>]*class="[^"]*\bwp-block-cgb-block-tab-container-block\b[^"]*"[^>]*>/i';
+	preg_match_all( $tag_regex, $content, $matches );
+
+	if ( ! $matches ) {
+		return $content;
+	}
+
+	$menu = '<div class="tab-container-block-menu"><ul>';
 
 	// grab the data-menu-title and id from each tag to construct the menu.
+	foreach ( $matches[0] as $match ) {
+		preg_match( '/id="([^"]*)"/i', $match, $id );
+		preg_match( '/data-menu-title="([^"]*)"/i', $match, $menu_title );
+
+		$menu .= '<li><a href="#' . $id[1] . '">' . $menu_title[1] . '</a></li>';
+	}
+
+	$menu .= '</ul></div>';
 
 	// add the menu markup to the end of $content.
-
-	return $content;
+	return $content . $menu;
 }
 add_filter( 'the_content', 'tcb_content_filter' );
