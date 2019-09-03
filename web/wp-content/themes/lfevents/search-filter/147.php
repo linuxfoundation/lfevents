1596350fe0b12ffaf9ce27223ed3b6e713b192fd
cjyabraham <cjyabraham@gmail.com>
2019-09-03 14:15:23 +0700

    Duplicating Community Events template temporarily for the live site

---
+++ web/wp-content/themes/lfevents/search-filter/147.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Search & Filter Pro Results Template
+ *
+ * @package   Search_Filter
+ * @author    Ross Morsali
+ * @link      https://searchandfilter.com
+ * @copyright 2018 Search & Filter
+ *
+ * Note: these templates are not full page templates, rather
+ * just an encaspulation of the your results loop which should
+ * be inserted in to other pages by using a shortcode - think
+ * of it as a template part
+ *
+ * This template is an absolute base example showing you what
+ * you can do, for more customisation see the WordPress docs
+ * and using template tags -
+ *
+ * http://codex.wordpress.org/Template_Tags
+ */
+
+global $post;
+
+echo '<div class="grid-x grid-margin-x">';
+
+if ( $query->have_posts() ) {
+	$y = 0;
+	$month = 0;
+	while ( $query->have_posts() ) {
+		$query->the_post();
+		$dt_date_start = new DateTime( get_post_meta( $post->ID, 'lfes_community_date_start', true ) );
+		$dt_date_end = new DateTime( get_post_meta( $post->ID, 'lfes_community_date_end', true ) );
+
+		$dt_date_end_1d_after = new DateTime( get_post_meta( $post->ID, 'lfes_community_date_end', true ) );
+		$dt_date_end_1d_after->add( new DateInterval( 'P1D' ) );
+		$dt_now = new DateTime( 'now' );
+		if ( $dt_date_end_1d_after < $dt_now ) {
+			// event has passed and we should set it to draft.
+			wp_update_post(
+				array(
+					'ID'          => $post->ID,
+					'post_status' => 'draft',
+				)
+			);
+			continue;
+		}
+
+		if ( ( 0 == $y ) || ( $y < (int) $dt_date_start->format( 'Y' ) ) ) {
+			$y = (int) $dt_date_start->format( 'Y' );
+			echo '<h2 class="cell event-calendar-year">' . esc_html( $y ) . '</h2>';
+			$month = (int) $dt_date_start->format( 'm' );
+			echo '<h3 class="cell event-calendar-month">' . esc_html( $dt_date_start->format( 'F' ) ) . '</h3>';
+		} elseif ( ( 0 == $month ) || ( $month < (int) $dt_date_start->format( 'm' ) ) ) {
+			$month = (int) $dt_date_start->format( 'm' );
+			echo '<h3 class="cell event-calendar-month">' . esc_html( $dt_date_start->format( 'F' ) ) . '</h3>';
+		}
+		?>
+		<article id="post-<?php the_ID(); ?>" class="cell medium-6 callout">
+			<h4 class="h5 medium-margin-right small-margin-bottom line-height-tight"><strong>
+			<?php
+			echo '<a href="' . esc_html( get_post_meta( $post->ID, 'lfes_community_external_url', true ) ) . '">' . esc_html( get_the_title() ) . '</a>';
+			?>
+			</strong></h4>
+			<p class="event-meta text-small small-margin-bottom">
+				<span class="date small-margin-right">
+					<svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="icon--inline"><g class="fa-group"><path fill="currentColor" d="M0 192v272a48 48 0 0 0 48 48h352a48 48 0 0 0 48-48V192zm128 244a12 12 0 0 1-12 12H76a12 12 0 0 1-12-12v-40a12 12 0 0 1 12-12h40a12 12 0 0 1 12 12zm0-128a12 12 0 0 1-12 12H76a12 12 0 0 1-12-12v-40a12 12 0 0 1 12-12h40a12 12 0 0 1 12 12zm128 128a12 12 0 0 1-12 12h-40a12 12 0 0 1-12-12v-40a12 12 0 0 1 12-12h40a12 12 0 0 1 12 12zm0-128a12 12 0 0 1-12 12h-40a12 12 0 0 1-12-12v-40a12 12 0 0 1 12-12h40a12 12 0 0 1 12 12zm128 128a12 12 0 0 1-12 12h-40a12 12 0 0 1-12-12v-40a12 12 0 0 1 12-12h40a12 12 0 0 1 12 12zm0-128a12 12 0 0 1-12 12h-40a12 12 0 0 1-12-12v-40a12 12 0 0 1 12-12h40a12 12 0 0 1 12 12z" class="fa-secondary"></path><path fill="currentColor" d="M448 112v48H0v-48a48 48 0 0 1 48-48h48V16a16 16 0 0 1 16-16h32a16 16 0 0 1 16 16v48h128V16a16 16 0 0 1 16-16h32a16 16 0 0 1 16 16v48h48a48 48 0 0 1 48 48z" class="fa-primary"></path></g></svg>
+					<?php echo esc_html( jb_verbose_date_range( $dt_date_start, $dt_date_end ) ); ?>
+				</span>
+
+				<span class="country">
+					<svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="icon--inline"><path fill="currentColor" d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z" class=""></path></svg>
+					<?php
+					$country = wp_get_post_terms( $post->ID, 'lfevent-country' );
+					if ( $country ) {
+						$country = $country[0]->name;
+						$city = get_post_meta( $post->ID, 'lfes_community_city', true );
+						if ( $city ) {
+							$city .= ', ';
+						}
+						echo esc_html( $city ) . esc_html( $country );
+					}
+					?>
+				</span>
+
+			</p>
+		</article>
+		<?php
+	}
+} else {
+	echo 'No Results Found';
+}
+echo '</div>';
+?>
