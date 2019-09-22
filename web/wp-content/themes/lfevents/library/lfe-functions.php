f91e909badddf2fdc0c6e2fcb22cdbd1db377832
James Hunt <jim@thetwopercent.co.uk>
2019-09-22 13:48:07 +0800

    Move Gravity Forms to footer - related to #265 fixes

--- web/wp-content/themes/lfevents/library/lfe-functions.php
+++ web/wp-content/themes/lfevents/library/lfe-functions.php
@@ -470,3 +470,28 @@ function remove_head_scripts() {
 	}
 	add_action( 'wp_enqueue_scripts', 'remove_head_scripts' );
 }
+
+/**
+ * Fix Gravity Forms loading its scripts too early
+ */
+
+function gf_init_scripts()
+{
+    return true;
+}
+add_filter('gform_init_scripts_footer', 'gf_init_scripts');
+
+/**
+ *  Gravity Forms inline JS to footer
+ * */
+function wrap_gform_cdata_open($content = '') {
+	$content = 'document.addEventListener( "DOMContentLoaded", function() { ';
+			return $content;
+	}
+add_filter('gform_cdata_open', 'wrap_gform_cdata_open');
+
+function wrap_gform_cdata_close($content = '') {
+	$content = ' }, false );';
+	return $content;
+}
+add_filter('gform_cdata_close', 'wrap_gform_cdata_close');
