177bcaa8e741acee98143ad32d3550addd14b7ad
James Hunt <jim@thetwopercent.co.uk>
2019-09-22 17:56:10 +0800

    Changing singular.php hero image to srcset

--- web/wp-content/themes/lfevents/singular.php
+++ web/wp-content/themes/lfevents/singular.php
@@ -40,22 +40,27 @@ if ( $logo ) {
 ?>
 
 <div data-sticky-container>
-	<header class="event-header sticky" data-sticky data-sticky-on="large" data-options="marginTop:0;" style="<?php echo esc_html( $background_style . $text_style ); ?>">
+  <header class="event-header sticky" data-sticky data-sticky-on="large"
+          data-options="marginTop:0;"
+          style="<?php echo esc_html( $background_style . $text_style ); ?>">
 
-		<div class="pre-nav">
-			<?php
+    <div class="pre-nav">
+      <?php
 			echo '<a class="event-home-link" href="' . get_permalink( $parent_id ) . '">' . $event_link_content . '</a>'; //phpcs:ignore
 			?>
 
-			<button class="menu-toggler button alignright" data-toggle="event-menu">
-				<span class="hamburger-icon"></span>
-			</button>
-		</div>
+      <button class="menu-toggler button alignright" data-toggle="event-menu">
+        <span class="hamburger-icon"></span>
+      </button>
+    </div>
 
-		<nav id="event-menu" class="event-menu show-for-large" data-toggler="show-for-large">
-			<ul class="event-menu-list">
-				<li class="page_item event-home-link" id="popout-header-link"><a href="<?php echo esc_url( get_permalink( $parent_id ) ); ?>" style="background-color:<?php echo $menu_color; ?>;"><?php echo $event_link_content; //phpcs:ignore ?></a></li>
-				<?php
+    <nav id="event-menu" class="event-menu show-for-large" data-toggler="show-for-large">
+      <ul class="event-menu-list">
+        <li class="page_item event-home-link" id="popout-header-link"><a
+             href="<?php echo esc_url( get_permalink( $parent_id ) ); ?>"
+             style="background-color:<?php echo $menu_color; ?>;"><?php echo $event_link_content; //phpcs:ignore ?></a>
+        </li>
+        <?php
 				if ( $menu_color_3 ) {
 					$background_style_solid = 'background: ' . $menu_color_3 . ';';
 				} else {
@@ -67,24 +72,25 @@ if ( $logo ) {
 				}
 				lfe_get_other_events( $parent_id, $background_style_solid, $menu_text_color );
 				?>
-			</ul>
-		</nav>
+      </ul>
+    </nav>
 
-	</header>
+  </header>
 </div>
 
 <div class="main-container">
-	<div class="main-grid">
-		<main class="main-content-full-width">
-			<?php
+  <div class="main-grid">
+    <main class="main-content-full-width">
+      <?php
 			while ( have_posts() ) :
 				the_post();
 
 				if ( $post->post_parent ) {
+
 					if ( has_post_thumbnail() ) {
-						$post_thumbnail_url = get_the_post_thumbnail_url();
+						$post_thumbnail_url = wp_get_attachment_image(get_post_thumbnail_id($post->ID), '', false, array('class' => 'fp-large'));
 					} else {
-						$post_thumbnail_url = get_the_post_thumbnail_url( $parent_id );
+						$post_thumbnail_url = wp_get_attachment_image(get_post_thumbnail_id($parent_id), '', false, array('class' => 'fp-large'));
 					}
 					if ( 'white' == $menu_text_color ) {
 						$subpage_header_text_color = 'black';
@@ -95,17 +101,25 @@ if ( $logo ) {
 					}
 					$subpage_header_style = 'color:' . $subpage_header_text_color . '; background-color: ' . $subpage_header_background_color . '; -webkit-text-fill-color: transparent; background: -webkit-gradient(linear,left top,right bottom,from(' . $menu_color_2 . '),to(' . $menu_color . ')); background: -o-gradient(linear,left top,right bottom,from(' . $menu_color_2 . '),to(' . $menu_color . ')); -webkit-background-clip: text;';
 					?>
-					<header class="event-subpage-header" style="background-image:url(<?php echo esc_html( $post_thumbnail_url ); ?>); <?php echo esc_html( $text_style ); ?>">
-						<div class="overlay" style="background: linear-gradient(90deg, <?php echo esc_html( $menu_color_2 ); ?> 0%, <?php echo esc_html( $menu_color ); ?> 100%); <?php echo esc_html( $text_style ); ?>"></div>
-						<h1 style="background-color: <?php echo esc_html( $subpage_header_background_color ); ?>;">
-							<span style="<?php echo esc_html( $subpage_header_style ); ?>">
-								<?php the_title(); ?>
-							</span>
-						</h1>
-					</header>
-					<?php
-				}
 
+      <header class="event-subpage-header background-image-wrapper"
+              style="<?php echo esc_html( $text_style ); ?>">
+        <div class="overlay"
+             style="background: linear-gradient(90deg, <?php echo esc_html( $menu_color_2 ); ?> 0%, <?php echo esc_html( $menu_color ); ?> 100%); <?php echo esc_html( $text_style ); ?>">
+        </div>
+        <figure class="figure-container">
+          <?php echo $post_thumbnail_url; ?>
+        </figure>
+
+        <h1 class="content-wrapper"
+            style="background-color: <?php echo esc_html( $subpage_header_background_color ); ?>;">
+          <span style="<?php echo esc_html( $subpage_header_style ); ?>">
+            <?php the_title(); ?>
+          </span>
+        </h1>
+      </header>
+      <?php
+				}
 				if ( is_page_template( 'page-templates/multi-part-page.php' ) ) {
 					get_template_part( 'template-parts/content', 'multi-part-page' );
 				} else {
@@ -114,16 +128,16 @@ if ( $logo ) {
 				lfe_get_sponsors( $parent_id );
 			endwhile;
 			?>
-		</main>
-	</div>
+    </main>
+  </div>
 </div>
 
 <div class="event-footer"
-	style="background: linear-gradient(90deg, <?php echo esc_html( $menu_color_2 ); ?> 0%, <?php echo esc_html( $menu_color ); ?> 100%); <?php echo esc_html( $text_style ); ?>">
-	<h3 class="event-social-links-header">Follow Us</h3>
-	<ul class="event-social-links">
+     style="background: linear-gradient(90deg, <?php echo esc_html( $menu_color_2 ); ?> 0%, <?php echo esc_html( $menu_color ); ?> 100%); <?php echo esc_html( $text_style ); ?>">
+  <h3 class="event-social-links-header">Follow Us</h3>
+  <ul class="event-social-links">
 
-		<?php
+    <?php
 		$wechat = get_post_meta( $parent_id, 'lfes_wechat', true );
 		$linkedin = get_post_meta( $parent_id, 'lfes_linkedin', true );
 		$qq = get_post_meta( $parent_id, 'lfes_qq', true );
@@ -156,7 +170,7 @@ if ( $logo ) {
 			echo '<li><a target="_blank" href="' . esc_html( $twitter ) . '"><svg class="social-icon--twitter" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"/></svg></a></li>';
 		}
 		?>
-	</ul>
+  </ul>
 </div>
 
 <?php
