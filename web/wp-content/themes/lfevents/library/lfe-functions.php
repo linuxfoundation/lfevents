7cdf4de1a78fc110316d10f32393f9e4ee921269
James Hunt <jim@thetwopercent.co.uk>
2019-10-04 11:13:10 +0800

    Adding back in functions

--- web/wp-content/themes/lfevents/library/lfe-functions.php
+++ web/wp-content/themes/lfevents/library/lfe-functions.php
@@ -441,3 +441,52 @@ function change_to_preconnect_resource_hints( $hints, $relation_type ) {
 	return $hints;
 }
 add_filter( 'wp_resource_hints', 'change_to_preconnect_resource_hints', 10, 2 );
+
+/**
+ * Fix Gravity Forms loading its scripts too early.
+ */
+function gf_init_scripts() {
+	return true;
+}
+add_filter( 'gform_init_scripts_footer', 'gf_init_scripts' );
+
+/**
+ *  Gravity Forms inline JS to footer
+ *
+ * @param string $content returns the cdata.
+ */
+function wrap_gform_cdata_open( $content = '' ) {
+	$content = 'document.addEventListener( "DOMContentLoaded", function() { ';
+			return $content;
+}
+add_filter( 'gform_cdata_open', 'wrap_gform_cdata_open' );
+
+/**
+ *  Gravity Forms inline JS to footer
+ *
+ * @param string $content returns the end of cdata.
+ */
+function wrap_gform_cdata_close( $content = '' ) {
+	$content = ' }, false );';
+	return $content;
+}
+add_filter( 'gform_cdata_close', 'wrap_gform_cdata_close' );
+
+/* Will only run on front end of site */
+if ( ! is_admin() ) {
+	/**
+	 * Make all JS defer onload (in conjunction with moving jQuery to footer).
+	 *
+	 * @param string $url the URL.
+	 */
+	function defer_parsing_of_js( $url ) {
+		if ( false === strpos( $url, '.js' ) ) {
+			return $url;
+		}
+		if ( strpos( $url, 'jquery-3.4.1.min.js' ) ) {
+			return $url;
+		}
+		return "$url' defer";
+	}
+	add_filter( 'clean_url', 'defer_parsing_of_js', 11, 1 );
+}
