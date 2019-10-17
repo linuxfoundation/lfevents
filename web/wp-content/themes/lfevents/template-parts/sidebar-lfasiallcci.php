bb406877c14effe590a9236caf3801ce5b1aea49
cjyabraham <cjyabraham@gmail.com>
2019-10-17 09:45:19 +0700

    added chinese translations to sidebar

--- web/wp-content/themes/lfevents/template-parts/sidebar-lfasiallcci.php
+++ web/wp-content/themes/lfevents/template-parts/sidebar-lfasiallcci.php
@@ -8,4 +8,5 @@
 
 ?>
 <p><img src="<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/' . foundationpress_asset_path( 'wechat_qr_lfasia.png' ); //phpcs:ignore ?>"></p>
+<p>我们举办的活动以教育和互联为目的，将开发人员和其他关键技术人员，管理和从事开源技术的操作人员聚集在一起。如需任何查询，<a href="/contact">请与我们联系</a>。</p>
 <p>The events we produce bring together developers and other key technologists, executives and operators working in open source technology for the purpose of education and networking. For any inquiries, please contact <a href="/contact">contact us</a>.</p>
