c85f5b90d23ce17f3a1bac09881cfd1c0dc387a7
cjyabraham <cjyabraham@gmail.com>
2019-10-30 17:19:09 +0700

    remove hamburger menu

--- web/wp-content/themes/lfevents/singular.php
+++ web/wp-content/themes/lfevents/singular.php
@@ -87,13 +87,7 @@ if ( ! $splash_page ) {
 	
 	<div data-sticky-container>
 		<header class="main-header sticky" data-sticky data-sticky-on="large" data-options="marginTop:0;">
-	
-			<button class="menu-toggler button alignright hide-for-large" data-toggle="main-menu">
-				<span class="hamburger-icon"></span>
-			</button>
-	
-			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/' . foundationpress_asset_path( $home_img ); //phpcs:ignore ?>"></a>
-	
+			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/' . foundationpress_asset_path( $home_img ); //phpcs:ignore ?>"></a>	
 		</header>
 	</div>
 	<?php
