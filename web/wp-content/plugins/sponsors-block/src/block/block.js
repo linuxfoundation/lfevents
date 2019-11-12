0e5d6a71d8dad8f359622d38105f81bf5d178bc8
cjyabraham <cjyabraham@gmail.com>
2019-11-12 22:49:22 +0700

    backed out change that was causing error

--- web/wp-content/plugins/sponsors-block/src/block/block.js
+++ web/wp-content/plugins/sponsors-block/src/block/block.js
@@ -176,7 +176,7 @@ registerBlockType( 'cgb/sponsors-block', {
           if ( image.description ) {
             return (
               <div class="sponsors-logo-item">
-                <a href={image.description} target="_blank">
+                <a href={image.description}>
                   <img
                     src={image.url}
                     alt={image.alt}
