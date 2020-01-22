b1e9be7f69304d3cf7cd44292d41983b42a6b0ab
cjyabraham <cjyabraham@gmail.com>
2020-01-22 15:22:08 +0700

    rolling wp media library categories plugin to v 2.7 due to a bug in the new version

--- web/wp-content/plugins/wp-media-library-categories/js/wpmlcp-media-views.js
+++ web/wp-content/plugins/wp-media-library-categories/js/wpmlcp-media-views.js
@@ -1004,10 +1004,7 @@ window.wpmlc = window.wpmlc || { l10n: {} };
 
         createSingle: function() {
 
-            // argument models not empty?
-            if ( arguments[1].models.length > 0 ) {
-                original.AttachmentsBrowser.createSingle.apply( this, arguments );
-            }
+            original.AttachmentsBrowser.createSingle.apply( this, arguments );
 
             if ( this.controller.isModeActive( 'wpmlc-grid' ) ) {
 
