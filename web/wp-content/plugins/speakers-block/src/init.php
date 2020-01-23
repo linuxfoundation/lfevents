2345e83faa4eb86cfdbc154d03dc04c4561f3b3b
fuerzastudio <contato@fuerzastudio.com.br>
2020-01-23 11:20:23 -0300

    [Speakers Block] Update bio position

--- web/wp-content/plugins/speakers-block/src/init.php
+++ web/wp-content/plugins/speakers-block/src/init.php
@@ -147,6 +147,7 @@ function speakers_block_callback( $attributes, $content ) {
 			$out .= '			<span class="name">' . esc_html( get_the_title() ) . '</span>';
 			$out .= '			<span class="title">' . get_post_meta( $id, 'lfes_speaker_title', true ) . '</span>';
 		}
+		$out .= '		</div>';
 		$out .= '			<div class="bio">';
 		$out .= '				<p>' . get_the_content() . '</p>';
 		$out .= '			</div>';
@@ -161,7 +162,6 @@ function speakers_block_callback( $attributes, $content ) {
 			$out .= '				<li><a href="' . $website . '"><svg class="social-icon--website" style="fill:' . $text_color . ';" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M448 80v352c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48V80c0-26.51 21.49-48 48-48h352c26.51 0 48 21.49 48 48zm-88 16H248.029c-21.313 0-32.08 25.861-16.971 40.971l31.984 31.987L67.515 364.485c-4.686 4.686-4.686 12.284 0 16.971l31.029 31.029c4.687 4.686 12.285 4.686 16.971 0l195.526-195.526 31.988 31.991C358.058 263.977 384 253.425 384 231.979V120c0-13.255-10.745-24-24-24z"/></svg></a></li>';
 		}
 		$out .= '			</ul>';
-		$out .= '		</div>';
 		$out .= '	</div>';
 		$out .= '</li>';
 	}
