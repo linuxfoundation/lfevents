afa7574d9e82caecedbbaeff2c8ccda9d96057f4
Chris Abraham <cjyabraham@gmail.com>
2020-01-24 11:19:40 +0700

    [Speakers Block] Change image markup and adjust CSS (#425)
    * [Speakers Block] Change image markup and adjust CSS
    
    * [Speakers Block] Fix CSS for images
    
    * [Speakers Block] Change crop and fix CSS for images
    
    * [Speakers Block] Fix bio position
    
    * [Speakers Block] Update bio position
    
    * [Speakers Block] Change social media icons position
    
    * [Speakers Block] Add new image size
    
    * reduce space before bio
    
    Co-authored-by: Fuerza Studio <52936031+fuerzastudio@users.noreply.github.com>

--- web/wp-content/themes/lfevents/library/responsive-images.php
+++ web/wp-content/themes/lfevents/library/responsive-images.php
@@ -14,7 +14,8 @@ add_image_size( 'fp-large', 1200 );
 add_image_size( 'fp-xlarge', 1920 );
 
 // profile pictures.
-add_image_size( 'profile-200', 200, 200 );
+add_image_size( 'profile-200', 200, 200, array( 'center', 'center' ) );
+add_image_size( 'profile-310', 310, 310, array( 'center', 'center' ) );
 
 // venue location image.
 add_image_size( 'venue', 600, 600, false );
