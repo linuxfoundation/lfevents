4bb77af313c1304288bc414da7ce3b74b010577d
fuerzastudio <contato@fuerzastudio.com.br>
2020-01-27 17:25:47 -0300

    [Text on image block] Refactoring code

--- web/wp-content/plugins/text-on-image-block/src/block/block.js
+++ web/wp-content/plugins/text-on-image-block/src/block/block.js
@@ -9,79 +9,66 @@
 import './style.scss';
 import './editor.scss';
 
-const { __ } = wp.i18n; // Import __() from wp.i18n
-const { registerBlockType, getBlockDefaultClassName } = wp.blocks; // Import registerBlockType() from wp.blocks
+const { __ } = wp.i18n;
+const { registerBlockType } = wp.blocks;
 const { RichText, MediaUpload } = wp.blockEditor;
 
-/**
- * Register: aa Gutenberg Block.
- *
- * Registers a new block provided a unique name and an object defining its
- * behavior. Once registered, the block is made editor as an option to any
- * editor interface where blocks are implemented.
- *
- * @link https://wordpress.org/gutenberg/handbook/block-api/
- * @param  {string}   name     Block name.
- * @param  {Object}   settings Block settings.
- * @return {?WPBlock}          The block, if it has been successfully
- *                             registered; otherwise `undefined`.
- */
 registerBlockType( 'cgb/block-text-on-image-block', {
-	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
-	title: __( 'Text on Image Block' ), // Block title.
-	icon: 'welcome-widgets-menus', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
-	category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
+	title: __( 'Text on Image Block' ),
+	icon: 'welcome-widgets-menus',
+	category: 'common',
 	keywords: [
 		__( 'text-on-image-block' ),
 	],
 	attributes: {
-        bodyContent: {
-            source: 'html',
-            selector: '.copy-bd'
-        },
-        imgUrl: {
-            type: 'string',
-            default: 'https://placehold.it/500'
-		}
+		bodyContent: {
+			type: 'string',
+			default: '',
+		},
+		imgUrl: {
+			type: 'string',
+			default: 'https://placehold.it/250?text=Sample+Image',
+		},
+		imgId: {
+			type: 'number',
+			default: 0,
+		},
 	},
 
-	/**
-	 * The edit function describes the structure of your block in the context of the editor.
-	 * This represents what the editor will render when the block is used.
-	 *
-	 * The "edit" property must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 */
 	edit: function( props ) {
-        const { className, setAttributes } = props;
+		const { className, setAttributes } = props;
 		const { attributes } = props;
 
-		function changeBodyContent(changes) {
-            setAttributes({
-                bodyContent: changes,
-			})
-        }
+		const onChangeContent = ( changes ) => {
+			setAttributes( {
+				bodyContent: changes,
+			} );
+		};
 
-        function selectImage(value) {
-            setAttributes({
-                imgUrl: value.sizes.full.url,
-            })
-        }
+		const handleBetterImageSize = ( value ) => {
+			const { sizes } = value;
+			return ( sizes.medium || sizes.full ).url;
+		};
+
+		const onSelectImage = ( value ) => {
+			setAttributes( {
+				imgId: value.id,
+				imgUrl: handleBetterImageSize( value ),
+			} );
+		};
+
 		return (
-			<div className={className}>
+			<div className={ className }>
 				<div className="media">
 					<MediaUpload
-						onSelect={selectImage}
-						render={ ({open}) => {
+						onSelect={ onSelectImage }
+						render={ ( { open } ) => {
 							return (
-								<button onClick={open}>
-									<img
-										src={attributes.imgUrl}
-										/>
+								<button onClick={ open }>
+									<img src={ attributes.imgUrl } alt="" />
 								</button>
 							);
-						}}
+						} }
 					/>
 				</div>
 				<div className="copy">
@@ -89,39 +76,14 @@ registerBlockType( 'cgb/block-text-on-image-block', {
 						className="copy-bd"
 						tagName="div"
 						placeholder="Enter text here that will float on top of the image."
-						value={attributes.bodyContent}
-						onChange={changeBodyContent}
-						/>
-				</div>
-			</div>
-		);
-	},
-
-	/**
-	 * The save function defines the way in which the different attributes should be combined
-	 * into the final markup, which is then serialized by Gutenberg into post_content.
-	 *
-	 * The "save" property must be specified and must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 */
-	save: function( props ) {
-		const className = getBlockDefaultClassName('cgb/block-text-on-image-block');
-		const { attributes } = props;
-
-		return (
-			<div class="alignfull lfe-image-and-text pull-right" style={ "background-image:url(" + attributes.imgUrl + ");"}>
-				<img src={attributes.imgUrl} />
-				<div class="text">
-					<blockquote>
-					<RichText.Content
-					className="copy-bd"
-					tagName="div"
-					value={attributes.bodyContent}
+						value={ attributes.bodyContent }
+						onChange={ onChangeContent }
+						keepPlaceholderOnFocus={ true }
 					/>
-					</blockquote>
 				</div>
 			</div>
 		);
 	},
+
+	save: () => null,
 } );
