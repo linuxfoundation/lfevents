698b836ac0b4d122ba4ab97491d0b86fb4e971a1
fuerzastudio <contato@fuerzastudio.com.br>
2020-02-12 09:52:42 -0300

    Added css property to trying disabled select on ios

--- web/wp-content/themes/lfevents/src/assets/js/mobile-select2.js
+++ web/wp-content/themes/lfevents/src/assets/js/mobile-select2.js
@@ -4,6 +4,10 @@ $(document).ready(function() {
       minimumResultsForSearch: Infinity
     });
 
+    $('select[data-combobox=1]').each(function(index, element){
+      element.style.pointerEvents = 'none';
+    });
+
     $('select[data-combobox=1]').on('select2:open', function(){
       $('.select2-search__field').prop('disabled', true);
     });
