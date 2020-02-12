7e719041b87af7d8df96dc276c602a8774a0425c
fuerzastudio <contato@fuerzastudio.com.br>
2020-02-12 11:06:53 -0300

    Change select behavior after search filter ajax

--- web/wp-content/themes/lfevents/src/assets/js/mobile-select2.js
+++ web/wp-content/themes/lfevents/src/assets/js/mobile-select2.js
@@ -1,15 +1,24 @@
 $(document).ready(function() {
   if (window.innerWidth < 768) {
-    $('select[data-combobox=1]').select2({
-      minimumResultsForSearch: Infinity
-    });
+    function fixMobile() {
+      $('select[data-combobox=1]').select2({
+        minimumResultsForSearch: Infinity
+      });
 
-    $('select[data-combobox=1]').each(function(index, element){
-      element.style.display = 'none';
-    });
+      $('select[data-combobox=1]').each(function(index, element) {
+        element.style.display = 'none';
+      });
 
-    $('select[data-combobox=1]').on('select2:open', function(){
-      $('.select2-search__field').prop('disabled', true);
+      $('select[data-combobox=1]').on('select2:open', function() {
+        $('.select2-search__field').prop('disabled', true);
+      });
+    }
+
+    fixMobile();
+
+    $(document).on("sf:ajaxfinish", ".searchandfilter", function() {
+      console.log('sf:ajaxfinish');
+      fixMobile();
     });
   }
 });
