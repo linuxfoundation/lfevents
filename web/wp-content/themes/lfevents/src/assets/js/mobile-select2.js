512a1ebadd46fa6670635120bdf4a56393d9fab2
fuerzastudio <contato@fuerzastudio.com.br>
2020-02-13 09:11:00 -0300

    Fix code sniffs

--- web/wp-content/themes/lfevents/src/assets/js/mobile-select2.js
+++ web/wp-content/themes/lfevents/src/assets/js/mobile-select2.js
@@ -1,23 +1,40 @@
-$(document).ready(function() {
-  if (window.innerWidth < 768) {
-    function fixMobileSelect2() {
-      $('select[data-combobox=1]').select2({
-        minimumResultsForSearch: Infinity
-      });
+/**
+ * JS code to run only on mobile view and change select2 behavior.
+ *
+ * @package WordPress
+ */
 
-      $('select[data-combobox=1]').each(function(index, element) {
-        element.style.display = 'none';
-      });
+$( document ).ready(
+	function() {
+		if ( window.innerWidth < 768 ) {
+			function fixMobileSelect2() {
+				$( 'select[data-combobox=1]' ).select2(
+					{ minimumResultsForSearch: Infinity }
+				);
 
-      $('select[data-combobox=1]').on('select2:open', function() {
-        $('.select2-search__field').prop('disabled', true);
-      });
-    }
+				$( 'select[data-combobox=1]' ).each(
+					function(index, element) {
+						element.style.display = 'none';
+					}
+				);
 
-    fixMobileSelect2();
+				$( 'select[data-combobox=1]' ).on(
+					'select2:open',
+					function() {
+						$( '.select2-search__field' ).prop( 'disabled', true );
+					}
+				);
+			}
 
-    $(document).on('sf:ajaxfinish', '.searchandfilter', function() {
-      fixMobileSelect2();
-    });
-  }
-});
+			fixMobileSelect2();
+
+			$( document ).on(
+				'sf:ajaxfinish',
+				'.searchandfilter',
+				function() {
+					fixMobileSelect2();
+				}
+			);
+		}
+	}
+);
