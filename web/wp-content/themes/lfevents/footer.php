5757d2ca399cacd96e84c77ffa1939522cd2a70c
cjyabraham <cjyabraham@gmail.com>
2020-02-18 10:59:30 +0700

    Accommodate Chinese footer with auto-update date

--- web/wp-content/themes/lfevents/footer.php
+++ web/wp-content/themes/lfevents/footer.php
@@ -12,7 +12,11 @@
 
 <footer class="site-footer">
 	<section id="text-2" class="widget widget_text"><div class="textwidget">
-		<p>Copyright &copy; <?php echo esc_html( gmdate( 'Y' ) ); ?> The Linux Foundation®. All rights reserved. The Linux Foundation has registered trademarks and uses trademarks. For a list of trademarks of The Linux Foundation, please see our <a href="https://www.linuxfoundation.org/trademark-usage/" target="_blank" rel="noopener">Trademark Usage</a> page. Linux is a registered trademark of Linus Torvalds.</p>
+		<?php if ( is_lfeventsci() ) { ?>
+			<p>Copyright &copy; <?php echo esc_html( gmdate( 'Y' ) ); ?> The Linux Foundation®. All rights reserved. The Linux Foundation has registered trademarks and uses trademarks. For a list of trademarks of The Linux Foundation, please see our <a href="https://www.linuxfoundation.org/trademark-usage/" target="_blank" rel="noopener">Trademark Usage</a> page. Linux is a registered trademark of Linus Torvalds.</p>
+		<?php } else { ?>
+			<p>Copyright <?php echo esc_html( gmdate( 'Y' ) ); ?> &copy; LF Asia, LLC. | info@lfasiallc.com, icp license, no. 京ICP备17074266号-6</p>
+		<?php } ?>		
 		<p>Forms on this site are protected by reCAPTCHA and the Google <a href="https://policies.google.com/privacy" target="_blank" rel="noopener">Privacy Policy</a> and <a href="https://policies.google.com/terms" target="_blank" rel="noopener">Terms of Service</a> apply.</p>
 	</div></section>
 	<?php dynamic_sidebar( 'footer-widgets' ); ?>
