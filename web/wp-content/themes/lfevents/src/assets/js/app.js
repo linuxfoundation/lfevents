3cad0ac48bcc919301615757ceb1fa85556c3ada
fuerzastudio <contato@fuerzastudio.com.br>
2020-02-27 16:35:08 -0300

    [Alert] Update alert block for mobile

--- web/wp-content/themes/lfevents/src/assets/js/app.js
+++ web/wp-content/themes/lfevents/src/assets/js/app.js
@@ -20,3 +20,42 @@ $( '.page_item_has_children a[href="#"]' ).click(
 		e.preventDefault();
 	}
 );
+function updateBannerSize() {
+  var w = window.innerWidth;
+  var charCount = document.querySelector('.alert-block').innerText.length
+
+  if(w <= 1200 && charCount > 130) {
+    $('html').addClass('double-top-banner');
+  }
+  if(w <= 900 && charCount > 105) {
+    $('html').addClass('double-top-banner');
+  }
+  if(w <= 600) {
+    if(charCount > 65) {
+      $('html').addClass('double-top-banner');
+    }
+    if(charCount > 155) {
+      $('html').removeClass('double-top-banner');
+      $('html').addClass('triple-top-banner');
+    }
+  }
+  if(w <= 415 && charCount > 105) {
+    $('html').removeClass('double-top-banner');
+    $('html').addClass('triple-top-banner');
+  }
+  if(w <= 375 && charCount > 90) {
+    $('html').removeClass('double-top-banner');
+    $('html').addClass('triple-top-banner');
+  }
+  if(w <= 320) {
+    if(charCount > 78) {
+      $('html').removeClass('double-top-banner');
+      $('html').addClass('triple-top-banner');
+    }
+    if(charCount > 118) {
+      $('html').removeClass('triple-top-banner');
+      $('html').addClass('quadruple-top-banner');
+    }
+  }
+}
+updateBannerSize();
