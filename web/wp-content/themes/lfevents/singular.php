0778e1c3ba216c8a9f7eff440715ca3fbda6d1d2
cjyabraham <cjyabraham@gmail.com>
2020-03-04 17:56:25 +0800

    adding past event banner

--- web/wp-content/themes/lfevents/singular.php
+++ web/wp-content/themes/lfevents/singular.php
@@ -97,6 +97,14 @@ if ( ! $splash_page ) {
 <div class="main-container">
   <div class="main-grid">
 	<main class="main-content-full-width">
+
+		<div class="alignfull text-center xlarge-padding-top large-padding-bottom" style="margin: 0 -0.625rem; background: <?php echo $menu_color_3; ?>; color: <?php echo $menu_text_color; ?>; box-shadow: inset 0 -0.125rem 0 0 rgba(0,0,0,.2);">
+		<h4 class="no-margin" style="">
+			<svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="icon--inline small-margin-right"><path fill="currentColor" d="M436 160H12c-6.6 0-12-5.4-12-12v-36c0-26.5 21.5-48 48-48h48V12c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v52h128V12c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v52h48c26.5 0 48 21.5 48 48v36c0 6.6-5.4 12-12 12zM12 192h424c6.6 0 12 5.4 12 12v260c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V204c0-6.6 5.4-12 12-12zm257.3 160l48.1-48.1c4.7-4.7 4.7-12.3 0-17l-28.3-28.3c-4.7-4.7-12.3-4.7-17 0L224 306.7l-48.1-48.1c-4.7-4.7-12.3-4.7-17 0l-28.3 28.3c-4.7 4.7-4.7 12.3 0 17l48.1 48.1-48.1 48.1c-4.7 4.7-4.7 12.3 0 17l28.3 28.3c4.7 4.7 12.3 4.7 17 0l48.1-48.1 48.1 48.1c4.7 4.7 12.3 4.7 17 0l28.3-28.3c4.7-4.7 4.7-12.3 0-17L269.3 352z" class=""></path></svg>
+			This event has passed. View the upcoming <a class="text-weight-bold" style="color:inherit;text-decoration:underline;">Kubernetes Forum Bengaluru</a>.
+		</h4>
+		</div>
+
 		<?php
 		while ( have_posts() ) :
 			the_post();
