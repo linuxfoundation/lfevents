3df171768429401e53a80d760bc11424a160291d
fuerzastudio <contato@fuerzastudio.com.br>
2020-03-25 15:36:12 -0300

    Update expiry buttons styles

--- web/wp-content/themes/lfevents/src/assets/js/button-with-expiry.js
+++ web/wp-content/themes/lfevents/src/assets/js/button-with-expiry.js
@@ -1,5 +1,13 @@
-const button = document.querySelector('.wp-block-button__link.has-text-color.has-white-color.has-background')
-const button_wrp = document.querySelector('.wp-block-lf-button-with-expiry')
-const button_style = button.getAttribute('style')
-button_wrp.setAttribute('style', button_style)
-button.setAttribute('style', '')
+/**
+ * Comment.
+ *
+ * @package Block LF Button With Expiry
+ */
+
+const button = document.querySelector( '.wp-block-button__link' )
+const button_wrp = document.querySelector( '.wp-block-lf-button-with-expiry' )
+const button_style = button.getAttribute( 'style' )
+const disabled = document.querySelector( '.wp-block-lf-button-with-expiry .disabled' )
+button_wrp.setAttribute( 'style', button_style )
+button.setAttribute( 'style', '' )
+disabled.parentElement.classList.add( 'disabled' )
