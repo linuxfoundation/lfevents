3a0143aec880c31a097105f602efb34b2479e913
cjyabraham <cjyabraham@gmail.com>
2020-04-07 11:54:23 +0800

    fixed bug with expiry buttons

--- web/wp-content/themes/lfevents/src/assets/js/button-with-expiry.js
+++ web/wp-content/themes/lfevents/src/assets/js/button-with-expiry.js
@@ -5,9 +5,11 @@
  */
 
 const button = document.querySelector( '.wp-block-button__link' )
-const button_wrp = document.querySelector( '.wp-block-lf-button-with-expiry' )
-const button_style = button.getAttribute( 'style' )
-const disabled = document.querySelector( '.wp-block-lf-button-with-expiry .disabled' )
-button_wrp.setAttribute( 'style', button_style )
-button.setAttribute( 'style', '' )
-disabled.parentElement.classList.add( 'disabled' )
+if ( button ) {
+    const button_wrp = document.querySelector( '.wp-block-lf-button-with-expiry' )
+    const button_style = button.getAttribute( 'style' )
+    const disabled = document.querySelector( '.wp-block-lf-button-with-expiry .disabled' )
+    button_wrp.setAttribute( 'style', button_style )
+    button.setAttribute( 'style', '' )
+    disabled.parentElement.classList.add( 'disabled' )
+}
