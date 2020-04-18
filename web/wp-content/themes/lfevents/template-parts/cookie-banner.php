415bbc1cb205a8380b3900e519dbe2e758ba38fe
James Hunt <jim@thetwopercent.co.uk>
2020-04-18 13:30:24 +0100

    Removing icon, matching button style

--- web/wp-content/themes/lfevents/template-parts/cookie-banner.php
+++ web/wp-content/themes/lfevents/template-parts/cookie-banner.php
@@ -25,6 +25,4 @@ if ( ! is_lfeventsci() ) {
 ?>
 
 <p id="cookie-banner"><?php echo esc_html( $cookies_text ); ?> <?php echo sprintf( '<a target="_blank" href="%s">%s</a>', esc_html( $cookies_link ), esc_html( $cookies_link_text ) ); ?><button id="cookie-banner-button" tabindex="0"
- role="button">
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1.959 17l-4.5-4.319 1.395-1.435 3.08 2.937 7.021-7.183 1.422 1.409-8.418 8.591z"/></svg>
-<?php echo esc_html( $cookies_button_text ); ?></button></p>
+ role="button"><?php echo esc_html( $cookies_button_text ); ?></button></p>
