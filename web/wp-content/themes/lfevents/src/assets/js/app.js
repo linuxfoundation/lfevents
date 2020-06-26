dfa4adf93a3263777eaaf2f2acfa6c081e0d2efc
James Hunt <jim@thetwopercent.co.uk>
2020-06-26 20:22:04 +0100

    Fixing linting in app.js

--- web/wp-content/themes/lfevents/src/assets/js/app.js
+++ web/wp-content/themes/lfevents/src/assets/js/app.js
@@ -44,21 +44,26 @@ $( document ).on(
 	}
 );
 
-$( document ).ready( function() {
+$( document ).ready(
+	function() {
 
-  var bg_images = document.getElementsByClassName('bg-images');
-  if (bg_images.length === 0) {
-    return;
-  }
+		var bg_images = document.getElementsByClassName( 'bg-images' );
+		if (bg_images.length === 0) {
+			return;
+		}
 
-  $('.bg-images > .bg-image:gt(0)').hide();
-	setInterval(function() {
-		$('.bg-images > .bg-image:first')
-		.fadeOut(1000)
-		.next()
-		.fadeIn(1000)
-		.end()
-		.appendTo('.bg-images');
-  }, 4000);
+		$( '.bg-images > .bg-image:gt(0)' ).hide();
+		setInterval(
+			function() {
+				$( '.bg-images > .bg-image:first' )
+				.fadeOut( 1000 )
+				.next()
+				.fadeIn( 1000 )
+				.end()
+				.appendTo( '.bg-images' );
+			},
+			4000
+		);
 
-});
+	}
+);
