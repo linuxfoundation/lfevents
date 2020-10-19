6eb0fd86b142960c441d5f563ddbad881ec25551
James Hunt <jim@thetwopercent.co.uk>
2020-10-19 11:12:32 +0200

    Adding file comment

--- web/wp-content/themes/lfevents/src/assets/js/responsive-table.js
+++ web/wp-content/themes/lfevents/src/assets/js/responsive-table.js
@@ -1,7 +1,17 @@
-$(document).ready(function () {
-  $('.is-style-table-with-column-headers table').ReStable({
-    rowHeaders: true, // Table has row headers.
-    maxWidth: 999, // Size to which the table become responsive.
-    keepHtml: true // Keep the html content of cells.
-});
-});
+/**
+ * Making tables responsive.
+ *
+ * @package WordPress
+ */
+
+$( document ).ready(
+	function () {
+		$( '.is-style-table-with-column-headers table' ).ReStable(
+			{
+				rowHeaders: true, // Table has row headers.
+				maxWidth: 999, // Size to which the table become responsive.
+				keepHtml: true // Keep the html content of cells.
+			}
+		);
+	}
+);
