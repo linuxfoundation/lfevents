60d97474cbefbe35f50ed3a403e29406538de6ff
cjyabraham <cjyabraham@gmail.com>
2020-10-27 15:47:57 +0800

    inspect cookies and doctor links for ShareASale tracking (#588)

--- web/wp-content/themes/lfevents/src/assets/js/app.js
+++ web/wp-content/themes/lfevents/src/assets/js/app.js
@@ -76,3 +76,38 @@ $( document ).ready(
 
 	}
 );
+
+
+// ShareASale tracking:
+// if sas_m_awin cookie is set then add its value as the sscid querystring param
+// on any links to cvent
+$( 'a[href*="cvent.com"]' ).each(
+	function () {
+		var link = $( this ).attr( 'href' );
+		var complement = '';
+		var cookie = readCookie("sas_m_awin");
+		var filling = JSON.parse(cookie);
+
+		if ( filling['clickId'] ) {
+			complement = 'sscid=' + filling['clickId']
+		}
+		if (link && complement) {
+			if (link.indexOf( '?' ) != -1) {
+				$( this ).attr( 'href', link + '&' + complement );
+			} else {
+				$( this ).attr( 'href', link + '?' + complement );
+			}
+		}
+	}
+);
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
