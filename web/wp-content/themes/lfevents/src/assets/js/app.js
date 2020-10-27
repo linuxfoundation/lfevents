cef5d4c3e57dd4164673c9f8285ca8ea19f7fb7a
cjyabraham <cjyabraham@gmail.com>
2020-10-27 15:52:48 +0800

    fixed code sniffs

--- web/wp-content/themes/lfevents/src/assets/js/app.js
+++ web/wp-content/themes/lfevents/src/assets/js/app.js
@@ -79,14 +79,13 @@ $( document ).ready(
 
 
 // ShareASale tracking:
-// if sas_m_awin cookie is set then add its value as the sscid querystring param
-// on any links to cvent
+// if sas_m_awin cookie is set then add its value as the sscid querystring param on any links to cvent.
 $( 'a[href*="cvent.com"]' ).each(
 	function () {
 		var link = $( this ).attr( 'href' );
 		var complement = '';
-		var cookie = readCookie("sas_m_awin");
-		var filling = JSON.parse(cookie);
+		var cookie = readCookie( "sas_m_awin" );
+		var filling = JSON.parse( cookie );
 
 		if ( filling['clickId'] ) {
 			complement = 'sscid=' + filling['clickId']
@@ -102,12 +101,17 @@ $( 'a[href*="cvent.com"]' ).each(
 );
 
 function readCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
+	var nameEQ = name + "=";
+	var ca = document.cookie.split( ';' );
+	var ca_len = ca.length;
+	for (var i = 0; i < ca_len; i++) {
+		var c = ca[i];
+		while (c.charAt( 0 ) == ' ') {
+			c = c.substring( 1, c.length );
+		}
+		if (c.indexOf( nameEQ ) == 0) {
+			return c.substring( nameEQ.length, c.length );
+		}
+	}
+	return null;
 }
