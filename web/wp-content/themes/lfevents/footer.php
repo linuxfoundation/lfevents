8b8584395efcfbe1e3989e5153ba875c98dde229
Chris Abraham <cjyabraham@gmail.com>
2021-02-16 15:26:20 +0800

    added footer to the main hub

--- web/wp-content/themes/lfevents/footer.php
+++ web/wp-content/themes/lfevents/footer.php
@@ -8,8 +8,105 @@
  * @since FoundationPress 1.0.0
  */
 
-?>
+if ( show_non_event_menu() ) :
+	?>
 
+	<section class="event-footer xlarge-padding-y is-style-lf-blue-gradient" role="footer">
+
+		<?php
+		$menu_text_color = 'white';
+		// setup the form defaults.
+		$form_title      = 'Join our mailing list to hear all the latest about events, news and more';
+		$form_privacy    = 'The Linux Foundation uses the information you provide to us to contact you about upcoming events. You may unsubscribe from these communications at any time. For more information, please see our <a target="_blank" rel="noopener" href="https://www.linuxfoundation.org/privacy/">Privacy Policy</a>.';
+
+		$allowed_elements = array(
+			'href'   => true,
+			'class'  => true,
+			'alt'    => true,
+			'rel'    => true,
+			'target' => true,
+		);
+		?>
+
+	<div class="event-footer-newsletter">
+		<p class="event-footer-newsletter__title"><?php echo wp_kses( $form_title, array( 'br' => array() ) ); ?></p>
+		<?php
+			echo do_shortcode( '[hubspot type=form portal=8112310 id=be35e462-1b9f-4499-9437-17f4d5c31ae5]' );
+		?>
+		<p class="event-footer-newsletter__privacy">
+		<?php
+		echo wp_kses(
+			$form_privacy,
+			array(
+				'a' => $allowed_elements,
+				'br' => array(),
+			)
+		);
+		?>
+		</p>
+	</div>
+
+		<div
+		class="event-footer-logo-social__wrapper <?php echo esc_html( $menu_text_color ); ?>">
+		<?php
+		$linkedin  = 'https://www.linkedin.com/company/the-linux-foundation/';
+		$youtube   = 'https://www.youtube.com/user/TheLinuxFoundation';
+		$facebook  = 'https://www.facebook.com/TheLinuxFoundation/';
+		$twitter   = 'https://twitter.com/linuxfoundation';
+		$instagram = 'https://www.instagram.com/linux_foundation';
+
+		echo '<ul class="event-footer-logo-social__icons ' . esc_html( $menu_text_color ) . '">';
+		if ( $wechat ) {
+			echo '<li>';
+			echo '<a data-toggle="wechat-dropdown">';
+			get_template_part( 'template-parts/svg/wechat' );
+			echo '</a>';
+			echo '<div class="dropdown-pane" id="wechat-dropdown" data-dropdown data-hover="true" data-hover-pane="true" data-hover-delay="0" data-position="top" data-alignment="center">' . wp_get_attachment_image( esc_html( $wechat ) ) . '</div>';
+			echo '</li>';
+		}
+		if ( $twitter ) {
+			echo '<li><a rel="noopener" title="Twitter" target="_blank" href="' . esc_html( $twitter ) . '">';
+			get_template_part( 'template-parts/svg/twitter' );
+			echo '</a></li>';
+		}
+		if ( $linkedin ) {
+			echo '<li><a rel="noopener" title="Linkedin" target="_blank" href="' . esc_html( $linkedin ) . '">';
+			get_template_part( 'template-parts/svg/linkedin' );
+			echo '</a></li>';
+		}
+		if ( $qq ) {
+			echo '<li><a rel="noopener" title="QQ" target="_blank" href="' . esc_html( $qq ) . '">';
+			get_template_part( 'template-parts/svg/qq' );
+			echo '</a></li>';
+		}
+		if ( $youtube ) {
+			echo '<li><a rel="noopener" title="YouTube" target="_blank" href="' . esc_html( $youtube ) . '">';
+			get_template_part( 'template-parts/svg/youtube' );
+			echo '</a></li>';
+		}
+		if ( $facebook ) {
+			echo '<li><a rel="noopener" title="Facebook" target="_blank" href="' . esc_html( $facebook ) . '">';
+			get_template_part( 'template-parts/svg/facebook' );
+			echo '</a></li>';
+		}
+		if ( $instagram ) {
+			echo '<li><a rel="noopener" title="Instagram" target="_blank" href="' . esc_html( $instagram ) . '">';
+			get_template_part( 'template-parts/svg/instagram' );
+			echo '</a></li>';
+		}
+		echo '</ul>';
+
+		if ( $hashtag ) {
+			?>
+		<div class="event-footer-logo-social__hashtag">
+			<p><?php echo esc_html( $hashtag ); ?></p>
+		</div>
+		<?php } ?>
+	</div>
+	</section>
+	<?php
+endif;
+?>
 <footer class="site-footer">
 	<section id="text-2" class="widget widget_text"><div class="textwidget">
 		<?php if ( is_lfeventsci() ) { ?>
