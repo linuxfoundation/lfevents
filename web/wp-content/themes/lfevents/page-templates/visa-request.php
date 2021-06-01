c1f6ddcb83964b8fea389b9585f92076ddf0d743
Chris Abraham <cjyabraham@gmail.com>
2021-06-01 08:14:02 +0800

    update visa request url

--- web/wp-content/themes/lfevents/page-templates/visa-request.php
+++ web/wp-content/themes/lfevents/page-templates/visa-request.php
@@ -25,7 +25,7 @@ get_template_part( 'template-parts/global-header' );
 
 						<div class="wp-block-group has-off-white-background-color has-background is-style-border"><div class="wp-block-group__inner-container">
 
-						<form id="sfmc-form1" action="https://cloud.email.thelinuxfoundation.org/Visa-Request-Submission">
+						<form id="sfmc-form1" action="https://x76by5x250.execute-api.us-east-2.amazonaws.com/dev/api/v1/sf/visaRequest">
 
 							<div class="grid-x grid-margin-x">
 								<div class="cell large-6">
