a1c3bd8b063bb94f4a35e8fa2127584fb7ac8090
James Hunt <jim@thetwopercent.co.uk>
2021-07-27 22:30:14 +0700

    Update Custom iFrame Block

--- web/wp-content/plugins/custom-iframe/build/index.asset.php
+++ web/wp-content/plugins/custom-iframe/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '66e34e3d8b17cc669641ebb89b5dd39c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '6fc044d0e8d64023402d848a3fe20788');
