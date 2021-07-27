c623f949de31c13a3fd7626f697c8eee81849f6a
James Hunt <jim@thetwopercent.co.uk>
2021-07-27 23:36:16 +0700

    Updating Sponsors Block dependencies

--- web/wp-content/plugins/sponsors-dynamic-block/src/init.php
+++ web/wp-content/plugins/sponsors-dynamic-block/src/init.php
@@ -32,9 +32,9 @@ function sponsors_dynamic_block_cgb_block_assets() { // phpcs:ignore
 	wp_register_script(
 		'sponsors-dynamic_block-cgb-block-js', // Handle.
 		plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ), // Block.build.js: We register the block here. Built with Webpack.
-		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ), // Dependencies, defined above.
-		null, // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
-		true // Enqueue the script in the footer.
+		array( 'wp-blocks', 'wp-i18n', 'wp-element' ),
+		null,
+		true
 	);
 
 	// Register block editor styles for backend.
