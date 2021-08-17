dbb8a76a339022513117e36746730b6453f682d2
Chris Abraham <cjyabraham@gmail.com>
2021-08-17 08:43:33 -0400

    remove "/" from disability

--- web/wp-content/themes/lfevents/page-templates/travel-fund-request.php
+++ web/wp-content/themes/lfevents/page-templates/travel-fund-request.php
@@ -220,7 +220,7 @@ get_template_part( 'template-parts/global-header' );
 										<input type="checkbox" name="group" id="groupLGBTQIA+" value="LGBTQIA+"><label for="groupLGBTQIA+">LGBTQIA+</label>
 										<input type="checkbox" name="group" id="groupWoman" value="Woman"><label for="groupWoman">Woman</label>
 										<input type="checkbox" name="group" id="groupPoC" value="Person of Color"><label for="groupPoC">Person of Color</label>
-										<input type="checkbox" name="group" id="groupPwD" value="Person with Disability"><label for="groupPwD">Person with Dis/ability</label>
+										<input type="checkbox" name="group" id="groupPwD" value="Person with Disability"><label for="groupPwD">Person with Disability</label>
 										<input type="checkbox" name="group" id="groupOther" onclick="toggleOtherInput(this)" value="Other"><label for="groupOther">Other</label>
 									</fieldset>
 								</div>
