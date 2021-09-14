33c681ba24bd0825e7a1a322764a9d7e63104aa0
James Hunt <10615884+thetwopct@users.noreply.github.com>
2021-09-15 01:10:41 +0700

    Updating Speakers Block

--- web/wp-content/mu-plugins/custom/lfevents/admin/js/lfevents-editor-only.js
+++ web/wp-content/mu-plugins/custom/lfevents/admin/js/lfevents-editor-only.js
@@ -72,20 +72,6 @@ wp.domReady(
 		]
 	);
 	wp.blocks.registerBlockStyle(
-		 'cgb/block-speakers-block',
-		[
-		{
-			name: 'default',
-			label: 'Default',
-			isDefault: true,
-		},
-		{
-			name: 'event-gradient',
-			label: 'Event Gradient',
-		}
-		]
-	);
-	wp.blocks.registerBlockStyle(
 		 'core/heading',
 		[
 		{
