0a753d6f3a09e6a787c9f25c379eccbe0dc8b881
James Hunt <10615884+thetwopct@users.noreply.github.com>
2022-09-13 01:34:16 +0700

    Updating LF Asia logo
    Signed-off-by: James Hunt <10615884+thetwopct@users.noreply.github.com>

--- web/wp-content/themes/lfevents/template-parts/header--logo.php
+++ web/wp-content/themes/lfevents/template-parts/header--logo.php
@@ -24,7 +24,7 @@
 		href="<?php echo esc_url( home_url( '/' ) ); ?>">
 		<img width="200" height="25" loading="eager" class="header--logo__image"
 			alt="<?php bloginfo( 'name' ); ?>"
-			src="<?php echo esc_html( get_stylesheet_directory_uri() . '/dist/assets/images/' . foundationpress_asset_path( 'lf-asia-color.svg' ) ); ?>">
+			src="<?php echo esc_html( get_stylesheet_directory_uri() . '/dist/assets/images/' . foundationpress_asset_path( 'logo_lfasiallc_white.svg' ) ); ?>">
 	</a>
 	<?php endif; ?>
 </div>
