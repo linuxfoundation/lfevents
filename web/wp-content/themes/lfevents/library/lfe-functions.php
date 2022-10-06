6d8d4ad8b9f0d7f1d0b5eeea6db56fad243c5640
Chris Abraham <cjyabraham@gmail.com>
2022-10-06 12:28:07 -0700

    update our favicon code to current standards
    Signed-off-by: Chris Abraham <cjyabraham@gmail.com>

--- web/wp-content/themes/lfevents/library/lfe-functions.php
+++ web/wp-content/themes/lfevents/library/lfe-functions.php
@@ -355,17 +355,16 @@ EOD;
 function lfe_insert_favicon() {
 	global $post;
 	$parent_id = lfe_get_event_parent_id( $post );
-
-	$out = '<link rel="icon" type="image/png" sizes="32x32" href="';
-
 	$favicon = get_post_meta( $parent_id, 'lfes_favicon', true );
-	if ( $favicon ) {
-		$out .= wp_get_attachment_url( $favicon );
-	} else {
-		$out .= get_stylesheet_directory_uri() . '/dist/assets/images/favicons/' . foundationpress_asset_path( 'favicon-32x32.png' );
-	}
 
-	$out .= '">';
+	if ( $favicon ) {
+		$out = '<link rel="icon" type="image/png" sizes="32x32" href="' . wp_get_attachment_url( $favicon ) . '">' . "\n";
+	} else {
+		$out  = '<link rel="icon" sizes="any" href="' . get_stylesheet_directory_uri() . '/dist/assets/images/favicons/favicon.ico">' . "\n";
+		$out .= '<link rel="icon" type="image/svg+xml" href="' . get_stylesheet_directory_uri() . '/dist/assets/images/favicons/favicon.svg">' . "\n";
+		$out .= '<link rel="apple-touch-icon" href="' . get_stylesheet_directory_uri() . '/dist/assets/images/favicons/apple-touch-icon.png">' . "\n";
+		$out .= '<link rel="manifest" href="' . get_stylesheet_directory_uri() . '/dist/assets/images/favicons/site.webmanifest">' . "\n";
+	}
 
 	echo $out; //phpcs:ignore
 }
