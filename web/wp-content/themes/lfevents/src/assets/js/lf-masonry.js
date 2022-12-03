d7f3175d4a6fa70407394cf94baba61b4f4577e9
Chris Abraham <cjyabraham@gmail.com>
2022-12-03 10:19:49 -0800

    fix code sniffs
    Signed-off-by: Chris Abraham <cjyabraham@gmail.com>

--- web/wp-content/themes/lfevents/src/assets/js/lf-masonry.js
+++ web/wp-content/themes/lfevents/src/assets/js/lf-masonry.js
@@ -5,19 +5,24 @@
  * @package WordPress
  */
 
+/**
+ * Applies masonry layout.
+ */
+
 function resizeGridItem(item){
-	var grid = document.getElementsByClassName("grid")[0];
-	var rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
-	var rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
-	var rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height+rowGap+32)/(rowHeight+rowGap));
-	item.style.gridRowEnd = "span "+rowSpan;
+	var grid = document.getElementsByClassName( "grid" )[0];
+	var rowHeight = parseInt( window.getComputedStyle( grid ).getPropertyValue( 'grid-auto-rows' ) );
+	var rowGap = parseInt( window.getComputedStyle( grid ).getPropertyValue( 'grid-row-gap' ) );
+	var rowSpan = Math.ceil( (item.querySelector( '.content' ).getBoundingClientRect().height + rowGap + 32) / (rowHeight + rowGap) );
+	item.style.gridRowEnd = "span " + rowSpan;
 }
- 
+
 function resizeAllGridItems(){
 	var x, allItems;
-	allItems = document.getElementsByClassName("item");
-	for(x=0;x<allItems.length;x++){
-	   resizeGridItem(allItems[x]);
+	allItems = document.getElementsByClassName( "item" );
+	var len = allItems.length;
+	for (x = 0;x < len;x++) {
+		resizeGridItem( allItems[x] );
 	}
 }
 
@@ -25,7 +30,6 @@ $( document ).ready(
 	function () {
 		var x, allItems;
 		window.onload = resizeAllGridItems();
-		window.addEventListener("resize", resizeAllGridItems);	
+		window.addEventListener( "resize", resizeAllGridItems );
 	}
 );
- 
