b4660a6b30dd1a2fc78315a7676f13b2db401ade
James Hunt <10615884+thetwopct@users.noreply.github.com>
2024-03-01 00:26:24 +0700

    Update header.php
    Signed-off-by: James Hunt <10615884+thetwopct@users.noreply.github.com>

--- web/wp-content/themes/lfevents/header.php
+++ web/wp-content/themes/lfevents/header.php
@@ -20,10 +20,12 @@ $all_classes = array(
 $classes     = implode( ' ', $all_classes );
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html class="no-js" <?php language_attributes(); ?>>
+	<head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
 	<?php wp_head(); ?>
 	<?php lfe_insert_favicon(); ?>
 	<?php lfe_insert_structured_data(); ?>
