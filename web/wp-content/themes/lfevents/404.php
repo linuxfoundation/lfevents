baf624ddcc2f6bd5c53d4acc8b986c26aa7e6736
Chris Abraham <cjyabraham@gmail.com>
2024-05-19 21:12:45 +0700

    try this
    Signed-off-by: Chris Abraham <cjyabraham@gmail.com>

--- web/wp-content/themes/lfevents/404.php
+++ web/wp-content/themes/lfevents/404.php
@@ -14,14 +14,14 @@ get_template_part( 'template-parts/header-global' );
 	<?php get_template_part( 'template-parts/non-event-hero' ); ?>
 	<div class="entry-content container wrap">
 		<div class="callout warning">
-			<p>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
+			<p>Yikes!</p>
 		</div>
 
 		<p>Please try the following!</p>
 		<ul>
 			<li>Check your spelling
 			</li>
-			<li>Return to the <a href="<?php echo esc_url(home_url() ); ?>">homepage</a></li>
+			<li>Return to the <a href="<?php echo esc_url( home_url() ); ?>">homepage</a></li>
 			<li>Click the <a href="javascript:history.back()">Back</a> button
 			</li>
 		</ul>
