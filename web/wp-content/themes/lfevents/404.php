b2ef1fcd2f3488f79c7a3f74e5692e4305e4dc16
Chris Abraham <cjyabraham@gmail.com>
2024-05-19 20:54:39 +0700

    test the new CI
    Signed-off-by: Chris Abraham <cjyabraham@gmail.com>

--- web/wp-content/themes/lfevents/404.php
+++ web/wp-content/themes/lfevents/404.php
@@ -17,11 +17,11 @@ get_template_part( 'template-parts/header-global' );
 			<p>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
 		</div>
 
-		<p>Please try the following:</p>
+		<p>Please try the following!</p>
 		<ul>
 			<li>Check your spelling
 			</li>
-			<li>Return to the <a href="<?php echo esc_url( home_url() ); ?>">homepage</a></li>
+			<li>Return to the <a href="<?php echo esc_url(home_url() ); ?>">homepage</a></li>
 			<li>Click the <a href="javascript:history.back()">Back</a> button
 			</li>
 		</ul>
