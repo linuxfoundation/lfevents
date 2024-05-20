c37c9f313ac7a198fef3904b0eb70b72b9b1b13c
Chris Abraham <cjyabraham@gmail.com>
2024-05-20 19:42:45 +0700

    test
    Signed-off-by: Chris Abraham <cjyabraham@gmail.com>

--- web/wp-content/themes/lfevents/404.php
+++ web/wp-content/themes/lfevents/404.php
@@ -17,7 +17,7 @@ get_template_part( 'template-parts/header-global' );
 			<p>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
 		</div>
 
-		<p>Please try the following:7</p>
+		<p>Please try the following:8</p>
 		<ul>
 			<li>Check your spelling
 			</li>
