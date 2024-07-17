52d421ee9d17e01a95c17220ba1c041212a8a065
Chris Abraham <cjyabraham@gmail.com>
2024-07-17 14:38:38 +0700

    Bulid the new block
    Signed-off-by: Chris Abraham <cjyabraham@gmail.com>

--- web/wp-content/plugins/speakers-block-2/src/index.js
+++ web/wp-content/plugins/speakers-block-2/src/index.js
@@ -2,6 +2,6 @@ import { registerBlockType } from '@wordpress/blocks';
 
 import Edit from './edit';
 
-registerBlockType( 'cgb/block-speakers-block', {
+registerBlockType( 'cgb/block-speakers-block-2', {
 	edit: Edit,
 } );
