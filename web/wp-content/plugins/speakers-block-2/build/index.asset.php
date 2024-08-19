a59b3bb06cae75252c8f19d9e431e5b05702119b
Chris Abraham <cjyabraham@gmail.com>
2024-08-19 18:12:11 +0700

    Fixed issue with hidden select drop-down on speakers block (fixes #933)
    Signed-off-by: Chris Abraham <cjyabraham@gmail.com>

--- web/wp-content/plugins/speakers-block-2/build/index.asset.php
+++ web/wp-content/plugins/speakers-block-2/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'b3203d2c3f86ef34cb65');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '32d9bb3de1d4323df7ef');
