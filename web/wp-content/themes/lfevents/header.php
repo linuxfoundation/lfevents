e385b734a63db5b3e77ce589f0283daec0b7664d
James Hunt <10615884+thetwopct@users.noreply.github.com>
2024-08-22 17:51:27 +0700

    Improve display of outlines site-wide, #673
    Signed-off-by: James Hunt <10615884+thetwopct@users.noreply.github.com>

--- web/wp-content/themes/lfevents/header.php
+++ web/wp-content/themes/lfevents/header.php
@@ -21,7 +21,7 @@ $classes     = implode( ' ', $all_classes );
 ?>
 
 <!DOCTYPE html>
-<html class="no-js" <?php language_attributes(); ?>>
+<html class="no-js no-focus-outline" <?php language_attributes(); ?>>
 	<head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
