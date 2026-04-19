557b1eb80ecba6251f2adbf745c0c3cb171d2284
Chris Abraham <cjyabraham@gmail.com>
2026-04-19 15:05:11 -0400

    added color pickers for the color overrides
    Signed-off-by: Chris Abraham <cjyabraham@gmail.com>

--- web/wp-content/plugins/sessionize-blocks/blocks/sessionize-schedule/build/index.asset.php
+++ web/wp-content/plugins/sessionize-blocks/blocks/sessionize-schedule/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'f76b9dcfac6f12bd2c4c');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'd039618a33c2adea90b0');
