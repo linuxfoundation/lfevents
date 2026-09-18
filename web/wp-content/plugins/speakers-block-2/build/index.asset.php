215f0bb72d8f54cbaab0d9a589a61800bbce7193
Chris Abraham <cjyabraham@gmail.com>
2026-09-18 11:42:39 +0700

    Fixes Speakers 2 block and Sponsors block for WP 7.1
    Signed-off-by: Chris Abraham <cjyabraham@gmail.com>

--- web/wp-content/plugins/speakers-block-2/build/index.asset.php
+++ web/wp-content/plugins/speakers-block-2/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks'), 'version' => '19787af58ebe2e768e3b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks'), 'version' => '5a8be5d2ca8aaf76177f');
